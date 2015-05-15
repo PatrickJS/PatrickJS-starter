3699dc5cccccf34d422f66935a5cae9c360ff25c
gdi2290 <github@gdi2290.com>
2015-05-14 19:02:22 -0700

    refactor: switch typescript

---
+++ src/app/components/app.ts
@@ -0,0 +1,13 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'app'
+})
+@View({
+  template: '<h1>Hello {{ name }}</h1>'
+})
+export class App {
+  constructor() {
+    this.name = 'Angular 2';
+  }
+}
