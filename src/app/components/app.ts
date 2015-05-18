72ffd064347906ac0f9f02b0a0473ca4ee312308
gdi2290 <github@gdi2290.com>
2015-05-17 21:20:36 -0700

    feat(app): include example Directive

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -1,14 +1,32 @@
-/// <reference path="../../../typings/angular2/angular2.d.ts" />
+/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="../../custom_typings/ng2.d.ts" />
 
-import {Component, View} from 'angular2/angular2';
+import {Component, View, Directive, ElementRef} from 'angular2/angular2';
 
+// Simple example directive
+@Directive({
+  selector: '[x-large]'
+})
+class XLarge {
+  constructor(public el: ElementRef) {
+    // simple dom manipulation to set font size to x-large
+    this.el.domElement.style.fontSize = 'x-large';
+  }
+}
+
+// Top Level Component
 @Component({
   selector: 'app'
 })
 @View({
-  template: '<h1>Hello {{ name }}</h1>'
+  directives: [ XLarge ],
+  template: `
+  <h1>Hello {{ name }}</h1>
+  <span x-large>Extra Large Font Directive</span>
+  `
 })
 export class App {
+  name: string;
   constructor() {
     this.name = 'Angular 2';
   }
