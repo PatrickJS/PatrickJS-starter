a9f161ded02a474c050e49241a54cee5640f4a13
gdi2290 <github@gdi2290.com>
2015-05-21 00:44:20 -0700

    refactor(app): clean up

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -1,53 +1,50 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 /// <reference path="../../custom_typings/ng2.d.ts" />
 
+// Angular 2
 import {Component, View} from 'angular2/angular2';
-import {RouteConfig, RouterOutlet, Router, RouterLink} from 'angular2/router';
+import {RouteConfig, RouterOutlet, RouterLink} from 'angular2/router';
 
+// We use a folder if we want separate files
 import {Home} from './home/home';
+// Otherwise we only use one file for a component
 import {Dashboard} from './dashboard';
-// Top Level Component
+
+// App: Top Level Component
 @Component({
   selector: 'app' // without [ ] means we are selecting the tag directly
 })
 @View({
-  directives: [
-    RouterOutlet,
-    RouterLink,
-    Home
-  ], // needed in order to tell Angular's compiler what's in the template
+  // needed in order to tell Angular's compiler what's in the template
+  directives: [ RouterOutlet, RouterLink, Home ],
   template: `
   <style>
-    .title {
-      font-family: Arial, Helvetica, sans-serif;
-    }
-    .nav {
-      display: inline;
-      list-style-type: none;
-    }
+    .title  { font-family: Arial, Helvetica, sans-serif; }
+    .nav    { display: inline; list-style-type: none; padding: 0;  background-color: #F8F8F8; }
+    .nav li { display: inline; }
+    main    { padding: 0.5em; }
   </style>
 
   <h1 class="title">Hello {{ name }}</h1>
 
   <ul class="nav">
-    <li>
-      <a router-link="home">Home</a>
-    </li>
-    <li>
-      <a router-link="dashboard">Dashboard</a>
-    </li>
+    <li><a router-link="home">Home</a></li>
+    |
+    <li><a router-link="dashboard">Dashboard</a></li>
   </ul>
 
-  <router-outlet></router-outlet>
+  <main>
+    <router-outlet></router-outlet>
+  </main>
   `
 })
 @RouteConfig([
-  { path: '/', as: 'home', component: Home},
+  { path: '/',          as: 'home',      component: Home },
   { path: '/dashboard', as: 'dashboard', component: Dashboard }
 ])
 export class App {
   name: string;
-  constructor(public router: Router) {
+  constructor() {
     this.name = 'Angular 2';
   }
 }
