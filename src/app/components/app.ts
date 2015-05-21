7b23e35c15ea21e6c56560be621b4f9c62ea35dd
gdi2290 <github@gdi2290.com>
2015-05-20 18:44:08 -0700

    refactor(app): include router

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -1,39 +1,53 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 /// <reference path="../../custom_typings/ng2.d.ts" />
 
-import {Component, View, Directive, ElementRef} from 'angular2/angular2';
+import {Component, View} from 'angular2/angular2';
+import {RouteConfig, RouterOutlet, Router, RouterLink} from 'angular2/router';
 
-// Simple example directive
-@Directive({
-  selector: '[x-large]' // using [ ] means selecting attributes
-})
-class XLarge {
-  constructor(public el: ElementRef) {
-    // simple dom manipulation to set font size to x-large
-    this.el.domElement.style.fontSize = 'x-large';
-  }
-}
-
+import {Home} from './home/home';
+import {Dashboard} from './dashboard';
 // Top Level Component
 @Component({
   selector: 'app' // without [ ] means we are selecting the tag directly
 })
 @View({
-  directives: [ XLarge ], // needed in order to tell Angular's compiler what's in the template
+  directives: [
+    RouterOutlet,
+    RouterLink,
+    Home
+  ], // needed in order to tell Angular's compiler what's in the template
   template: `
   <style>
     .title {
       font-family: Arial, Helvetica, sans-serif;
     }
+    .nav {
+      display: inline;
+      list-style-type: none;
+    }
   </style>
 
   <h1 class="title">Hello {{ name }}</h1>
-  <span x-large>Extra Large Font Directive lol</span>
+
+  <ul class="nav">
+    <li>
+      <a router-link="home">Home</a>
+    </li>
+    <li>
+      <a router-link="dashboard">Dashboard</a>
+    </li>
+  </ul>
+
+  <router-outlet></router-outlet>
   `
 })
+@RouteConfig([
+  { path: '/', as: 'home', component: Home},
+  { path: '/dashboard', as: 'dashboard', component: Dashboard }
+])
 export class App {
   name: string;
-  constructor() {
+  constructor(public router: Router) {
     this.name = 'Angular 2';
   }
 }
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,28 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../../custom_typings/ng2.d.ts" />
+import {Component, View, Directive, ElementRef} from 'angular2/angular2';
+
+// Simple example directive
+@Directive({
+  selector: '[x-large]' // using [ ] means selecting attributes
+})
+class XLarge {
+  constructor(public el: ElementRef) {
+    // simple dom manipulation to set font size to x-large
+    this.el.domElement.style.fontSize = 'x-large';
+  }
+}
+
+
+@Component({
+  selector: 'home'
+})
+@View({
+  directive: [ XLarge ],
+  template: require('./home.html') // using webpack require
+})
+export class Home {
+  constructor() {
+
+  }
+}
