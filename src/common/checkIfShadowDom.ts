19d6cfc5b6ebbeb7cac3bbbc13ce9df582124961
gdi2290 <github@gdi2290.com>
2015-05-24 16:54:10 -0700

    fix(checkIfShadowDom): update interface

--- src/common/checkIfShadowDom.ts
+++ src/common/checkIfShadowDom.ts
@@ -1,11 +1,16 @@
 /// <reference path="../../typings/tsd.d.ts" />
 /// <reference path="../custom_typings/ng2.d.ts" />
 import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/core';
+import {StyleUrlResolver} from 'angular2/src/render/dom/shadow_dom/style_url_resolver';
 import {bind} from 'angular2/di';
 import {document} from 'angular2/src/facade/browser';
 
 export var hasShadowDom = Boolean(document && document.body && document.body.createShadowRoot);
 
 export var shadowDomInjectables = [
-  (hasShadowDom) ? bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy) : []
+  (!hasShadowDom) ? [] :
+  bind(ShadowDomStrategy).toFactory(
+    styleUrlResolver => new NativeShadowDomStrategy(styleUrlResolver),
+    [StyleUrlResolver]
+  )
 ]
