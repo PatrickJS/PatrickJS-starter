b1c46a1efd2928838d27a46eccc7ca0d7239689e
gdi2290 <github@gdi2290.com>
2015-05-26 22:31:16 -0700

    feat(TodoService): ITodo interface

--- src/app/services/TodoService.ts
+++ src/app/services/TodoService.ts
@@ -1,35 +1,37 @@
 import {bind, Inject} from 'angular2/di';
 
 // Using TypeScript we can define our state interface
+interface ITodo {
+  value: string;
+  created_at: Date;
+  completed?: boolean;
+}
 interface ITodoState {
-  todos: Array<{
-    value: string,
-    created_at: Date
-  }>
+  todos: Array<ITodo>
 }
 
 // We can also make a TodoStore to manage cache/localStorage
 let _todoState:ITodoState = {
   todos: [
-    {value:'finish example', created_at: new Date()},
-    {value:'add tests', created_at: new Date()}
+    { value:'finish example', created_at: new Date() },
+    { value:'add tests',      created_at: new Date() }
   ]
 };
 
 // Our Todo Service
 export class TodoService {
-  private state: ITodoState; // we shouldn't access .state directly
+  private _state: ITodoState; // we shouldn't access .state directly
   constructor(@Inject('todoState') state) {
-    this.state = state;
+    this._state = state;
   }
 
   get(type) {
-    return (type) ? this.state[type] : this.state;
+    return (type) ? this._state[type] : this._state;
   }
 
   add(todo) {
     // Async call to server then save state
-    this.state.todos.push({
+    this._state.todos.push({
       value: todo,
       created_at: new Date()
     });
@@ -37,7 +39,7 @@ export class TodoService {
 
   remove(index) {
     // Async call to server then save state
-    this.state.todos.splice(index, 1);
+    this._state.todos.splice(index, 1);
   }
 
 }//TodoService
