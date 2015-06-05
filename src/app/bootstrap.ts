f5268685e285ba051cb58bd4053d3ffa776ad86e
gdi2290 <github@gdi2290.com>
2015-06-05 16:50:58 -0700

    refactor(bootstrap): more comments and add jit

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -4,21 +4,48 @@
 // Angular 2
 import {bootstrap} from 'angular2/angular2';
 
-// include any injectables
+// Angular's router injectables services/bindings
 import {routerInjectables} from 'angular2/router';
-import {shadowDomInjectables} from '../common/shadowDomInjectables';
+// Angular's form injectables services/bindings
+import {formInjectables} from 'common/formInjectables';
 
-// Our injectables Services
+// Our custom injectable that checks if ShadowDom is available to inject
+import {shadowDomInjectables} from 'common/shadowDomInjectables';
+
+// Our custom injectable that uses Just-In-Time change detection
+// import {jitInjectables} from 'common/changeDetectionInjectables';
+
+// Our collection of injectables services
 import {appServicesInjectables} from './services/services';
-// Top level component to bootstrap
+
+
+// Our top level component that holds all of our components
 import {App} from './components/app';
 
-// bootstrap the Angular app with bindings
-bootstrap(App, [
-  // define any componentInjectableBindings
-  routerInjectables,
-  // if we want to use ShadowDom
-  shadowDomInjectables,
-  // our servies
-  appServicesInjectables
-]);
+/*
+  Bootstrap our Angular app with our top level component `App`
+  and inject our global services/bindings into Angular's dependency injection
+*/
+bootstrap(
+  // Top Level Component
+  App,
+
+  // AppInjectors
+  [
+    // Our collection of services from /services
+    appServicesInjectables,
+
+
+    // Our custom injectable that checks if we have support for ShadowDom
+    shadowDomInjectables,
+
+    // if we want to use the Just-In-Time change detection
+    // jitInjectables,
+
+    // Angular's form builder service
+    formInjectables,
+
+    // Angular's router
+    routerInjectables
+  ]
+);
