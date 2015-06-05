dd5cee9736b5bec2cecc1ce96fff5d10de5f2340
gdi2290 <github@gdi2290.com>
2015-06-05 16:51:28 -0700

    refactor(shadowDomInjectables): use toClass

--- src/common/shadowDomInjectables.ts
+++ src/common/shadowDomInjectables.ts
@@ -1,15 +1,11 @@
 /// <reference path="../../typings/tsd.d.ts" />
 /// <reference path="../custom_typings/ng2.d.ts" />
-import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/core';
-import {StyleUrlResolver} from 'angular2/src/render/dom/shadow_dom/style_url_resolver';
+import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/render';
 import {bind} from 'angular2/di';
 import {document} from 'angular2/src/facade/browser';
 
 export var hasShadowDom = Boolean(document && document.body && document.body.createShadowRoot);
 
 export var shadowDomInjectables = (!hasShadowDom) ? [] : [
-  bind(ShadowDomStrategy).toFactory(
-    styleUrlResolver => new NativeShadowDomStrategy(styleUrlResolver),
-    [StyleUrlResolver]
-  )
+  bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy)
 ]
