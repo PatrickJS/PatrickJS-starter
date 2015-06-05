2ed17821c15d1cd7c3e23d2cc5d8b08e7a3e0161
gdi2290 <github@gdi2290.com>
2015-06-05 16:55:07 -0700

    refactor(TodoService): extend Store

--- src/app/services/TodoService.ts
+++ src/app/services/TodoService.ts
@@ -1,5 +1,5 @@
 import {bind, Inject} from 'angular2/di';
-
+import {Store} from './Store';
 // Using TypeScript we can define our state interface
 interface ITodo {
   value: string;
@@ -11,40 +11,50 @@ interface ITodoState {
 }
 
 // We can also make a TodoStore to manage cache/localStorage
-let _todoState:ITodoState = {
+let initialTodoState:ITodoState = {
   todos: [
     { value:'finish example', created_at: new Date() },
-    { value:'add tests',      created_at: new Date() }
+    { value:'add tests',      created_at: new Date() },
+    { value:'include development environment', created_at: new Date() },
+    { value:'include production environment',  created_at: new Date() }
   ]
 };
 
-// Our Todo Service
-export class TodoService {
-  private _state: ITodoState; // we shouldn't access .state directly
-  constructor(@Inject('todoState') state) {
-    this._state = state;
-  }
-
-  get(type) {
-    return (type) ? this._state[type] : this._state;
+// Our Todo Service that uses Store helper class for managing our state
+export class TodoService extends Store {
+  // we shouldn't access ._state or ._setState outside of the class
+  constructor(@Inject('initialTodoState') state: ITodoState) {
+    // use Store class as a helper
+    super(state);
   }
 
   add(todo) {
     // Async call to server then save state
-    this._state.todos.push({
+    var todos = this.get('todos');
+    todos.push({
       value: todo,
       created_at: new Date()
     });
+
+    // Always Replace state
+    this.set('todos', todos);
   }
 
   remove(index) {
     // Async call to server then save state
-    this._state.todos.splice(index, 1);
+    var todos = this.get('todos');
+    todos.splice(index, 1);
+
+    // Always Replace state
+    this.set({
+      todos: todos
+    });
+
   }
 
 }//TodoService
 
-export const todoInjectables = [
-  bind('todoState').toValue(_todoState),
+export var todoInjectables = [
+  bind('initialTodoState').toValue(initialTodoState),
   bind(TodoService).toClass(TodoService)
 ];
