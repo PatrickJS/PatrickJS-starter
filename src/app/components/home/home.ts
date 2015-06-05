46a3273c7298c639b3ee903dd766b244ee1e3c0a
gdi2290 <github@gdi2290.com>
2015-06-05 16:52:47 -0700

    feat(home): include directives

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -4,6 +4,12 @@
 // Angular 2
 import {Component, View} from 'angular2/angular2';
 
+// Directives
+import {coreDirectives} from 'angular2/directives';
+import {formDirectives} from 'angular2/forms';
+import {appDirectives} from 'app/directives/directives';
+
+
 // Use webpack to get files as a raw string using raw-loader
 let styles   = require('./home.css');
 let template = require('./home.html');
@@ -12,6 +18,7 @@ let template = require('./home.html');
   selector: 'home'
 })
 @View({
+  directives: [ coreDirectives, formDirectives, appDirectives ],
   // include our .html and .css file
   template:`<style>${styles}</style>\n${template}`
 })
