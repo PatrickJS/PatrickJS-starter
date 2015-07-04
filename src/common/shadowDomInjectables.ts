2ad9c5fc67f7438a86e8ae572cecac7c2f2c0711
gdi2290 <github@gdi2290.com>
2015-07-03 17:31:07 -0700

    feat(shadowDomInjectables): include more strategies

--- src/common/shadowDomInjectables.ts
+++ src/common/shadowDomInjectables.ts
@@ -1,10 +1,25 @@
 /// <reference path="../../typings/tsd.d.ts" />
-import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/render';
+import {
+  ShadowDomStrategy,
+  NativeShadowDomStrategy,
+  EmulatedScopedShadowDomStrategy,
+  EmulatedUnscopedShadowDomStrategy
+} from 'angular2/render';
 import {bind} from 'angular2/di';
 import {document} from 'angular2/src/facade/browser';
 
-export var hasShadowDom = Boolean(document && document.body && document.body.createShadowRoot);
+export function hasNativeShadowDom() {
+  return Boolean(document && document.body && document.body.createShadowRoot);
+}
 
-export var shadowDomInjectables = (!hasShadowDom) ? [] : [
+export var nativeShadowDomInjectables = !hasNativeShadowDom() ? [] : [
   bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy)
-]
+];
+
+export var emulatedScopedShadowDomInjectables = [
+  bind(ShadowDomStrategy).toClass(EmulatedScopedShadowDomStrategy)
+];
+
+export var emulatedUnscopedShadowDomInjectables = [
+  bind(ShadowDomStrategy).toClass(EmulatedUnscopedShadowDomStrategy)
+];
