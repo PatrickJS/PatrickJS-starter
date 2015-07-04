805e51ce9b00088d1b0a11423e57d1c0d5f7eaf6
gdi2290 <github@gdi2290.com>
2015-07-03 17:29:44 -0700

    feat(common): include location strategies

---
+++ src/common/locationInjectables.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../typings/tsd.d.ts" />
+import {bind} from 'angular2/di';
+import {
+  LocationStrategy,
+  HashLocationStrategy,
+  HTML5LocationStrategy
+} from 'angular2/router';
+
+export var html5locationInjectables = [
+  bind(LocationStrategy).toClass(HTML5LocationStrategy)
+];
+
+export var hashlocationInjectables = [
+  bind(LocationStrategy).toClass(HashLocationStrategy)
+];
