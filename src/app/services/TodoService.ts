63dc0e2b48acaf5e9996904bf7a9e357675ff64c
gdi2290 <github@gdi2290.com>
2015-07-03 17:28:08 -0700

    refactor(TodoService): remove Store

--- src/app/services/TodoService.ts
+++ src/app/services/TodoService.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 import {bind, Inject} from 'angular2/di';
-import {Store} from './Store';
+
 // Using TypeScript we can define our state interface
 interface ITodo {
   value: string;
@@ -14,36 +14,51 @@ interface ITodoState {
 // We can also make a TodoStore to manage cache/localStorage
 let initialTodoState:ITodoState = {
   todos: [
-    { value:'finish example', created_at: new Date() },
-    { value:'add tests',      created_at: new Date() },
-    { value:'include development environment', created_at: new Date() },
-    { value:'include production environment',  created_at: new Date() }
+    { value: 'finish example', created_at: new Date() },
+    { value: 'add tests',      created_at: new Date() },
+    { value: 'include development environment', created_at: new Date() },
+    { value: 'include production environment',  created_at: new Date() }
   ]
 };
 
 // Our Todo Service that uses Store helper class for managing our state
-export class TodoService extends Store {
+export class TodoService {
   // we shouldn't access ._state or ._setState outside of the class
+  _state: ITodoState;
   constructor(@Inject('initialTodoState') state: ITodoState) {
     // use Store class as a helper
-    super(state);
+    this._state = state;
+  }
+
+  get(type?: string) {
+    return (type) ? this._state[type] : this._state;
+  }
+
+  set(prop: any, value?: any) {
+    // .set({ todos: [] }) one argument to replace all of the state
+    // .set('todos', [])   two arguments only to replace a property in our state
+    this._state = (value === undefined) ? prop : Object.assign({}, {
+      [prop]: value
+    });
   }
 
   add(todo) {
     // Async call to server then save state
-    var todos = this.get('todos');
+
+    var todos = this.get('todos').slice(); // create array copy
     todos.push({
       value: todo,
       created_at: new Date()
     });
 
-    // Always Replace state
+    // You can use .set to replace state
     this.set('todos', todos);
   }
 
   remove(index) {
     // Async call to server then save state
-    var todos = this.get('todos');
+
+    var todos = this.get('todos').slice(); // create array copy
     todos.splice(index, 1);
 
     // Always Replace state
@@ -55,7 +70,8 @@ export class TodoService extends Store {
 
 }//TodoService
 
-export var todoInjectables = [
+// export our injectables for this module
+export var todoInjectables: Array<any> = [
   bind('initialTodoState').toValue(initialTodoState),
   bind(TodoService).toClass(TodoService)
 ];
--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -2,6 +2,6 @@
 import {todoInjectables} from './TodoService';
 
 
-export var appServicesInjectables:Array<any> = [
+export var appServicesInjectables: Array<any> = [
   todoInjectables
 ];
