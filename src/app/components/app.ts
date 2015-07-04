bb5f483d66ed5b0baaef1f0559cac4078db4c0a5
gdi2290 <github@gdi2290.com>
2015-07-03 17:22:12 -0700

    refactor(app): clean up and include more comments

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -1,10 +1,24 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
-// Angular 2
-import {Component, View, coreDirectives} from 'angular2/angular2';
-import {RouteConfig, RouterOutlet, RouterLink, Router} from 'angular2/router';
-import {BrowserLocation} from 'angular2/src/router/browser_location';
+/*
+ * Angular 2
+ */
+import {Component, View} from 'angular2/angular2';
+import {RouteConfig} from 'angular2/router';
 
+/*
+ * Directives
+ */
+// Import all of our custom app directives
+import {coreDirectives} from 'angular2/angular2';
+import {routerDirectives} from 'angular2/router';
+// import {formDirectives} from 'angular2/forms';
+import {formDirectives} from '../../common/formDirectives'; // current workaround fix
+import {appDirectives} from '../directives/directives';
+
+/*
+ * Components
+ */
 // We use a folder if we want separate files
 import {Home} from './home/home';
 // Otherwise we only use one file for a component
@@ -12,16 +26,28 @@ import {Dashboard} from './dashboard';
 // A simple example of a Component using a Service
 import {Todo} from './todo';
 
-// Import all of our custom app directives
-import {appDirectives} from '../directives/directives';
-
-// App: Top Level Component
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-  selector: 'app' // without [ ] means we are selecting the tag directly,
+  selector: 'app', // without [ ] means we are selecting the tag directly
 })
 @View({
   // needed in order to tell Angular's compiler what's in the template
-  directives: [ RouterOutlet, RouterLink, coreDirectives, appDirectives ],
+  directives: [
+    // Angular's core directives
+    coreDirectives,
+
+    // Angular's form directives
+    formDirectives,
+
+    // Angular's router
+    routerDirectives,
+
+    // Our collection of directives from /directives
+    appDirectives
+  ],
   template: `
   <style>
     .title  { font-family: Arial, Helvetica, sans-serif; }
@@ -29,21 +55,23 @@ import {appDirectives} from '../directives/directives';
     .nav li { display: inline; }
     main    { padding: 0.5em; }
   </style>
-
   <h1 class="title">Hello {{ name }}</h1>
 
   <ul class="nav">
-    <li><a router-link="home">Home</a></li>
+    <li><a [router-link]=" ['/home'] ">Home</a></li>
     |
-    <li><a router-link="dashboard">Dashboard</a></li>
+    <li><a [router-link]=" ['/dashboard'] ">Dashboard</a></li>
     |
-    <li><a router-link="todo">Todo</a></li>
+    <li><a [router-link]=" ['/todo'] ">Todo</a></li>
   </ul>
-
 
   <main>
     <router-outlet></router-outlet>
   </main>
+
+  <footer>
+    WebPack Angular 2 Starter by <a href="https://twitter.com/AngularClass">@AngularClass</a>
+  </footer>
   `
 })
 @RouteConfig([
@@ -53,11 +81,7 @@ import {appDirectives} from '../directives/directives';
 ])
 export class App {
   name: string;
-  constructor(router: Router, browserLocation: BrowserLocation) {
+  constructor() {
     this.name = 'Angular 2';
-
-    // we need to manually go to the correct uri until the router is fixed
-    let uri = browserLocation.path();
-    router.navigate(uri);
   }
 }
