17c8f6409ed480e721ab24020996865de30cf370
gdi2290 <github@gdi2290.com>
2015-07-14 10:59:53 -0700

    chore: update angular alpha.31

--- src/app/pipes/pipes.ts
+++ src/app/pipes/pipes.ts
@@ -11,26 +11,11 @@ import {PipeRegistry, Pipes, defaultPipes} from 'angular2/change_detection';
 import {capitalize} from './CapitalizePipe';
 import {rxAsync} from './RxPipe';
 
-var newPipesBindings;
-
-// Alpha.31
-if (Pipes) {
-  newPipesBindings = bind(Pipes).toValue(Pipes.append({
-    'async': rxAsync,
-    'capitalize': capitalize
-    // add more pipes to this Map
-  }));
-}
-// Alpha.30
-else if (PipeRegistry) {
-  // include the default pipes and our own
-  let replaceBindings = Object.assign({}, defaultPipes, {
-    'async': rxAsync,
-    'capitalize': capitalize
-    // add more pipes to this Map
-  });
-  newPipesBindings = bind(PipeRegistry).toValue(new PipeRegistry(replaceBindings));
-}
+var newPipesBindings = bind(Pipes).toValue(Pipes.append({
+  'async': rxAsync,
+  'capitalize': capitalize
+  // add more pipes to this Map
+}));
 
 export var appPipeInjectables = [
   newPipesBindings
