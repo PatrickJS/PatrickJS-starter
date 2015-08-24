d52bbd5731c66d6abe89ee49c0ccac2ae603ad24
gdi2290 <github@gdi2290.com>
2015-08-23 20:54:15 -0700

    chore(common): rename to bindings folder

--- src/bindings/change_detection_bindings.ts
+++ src/bindings/change_detection_bindings.ts
@@ -7,19 +7,19 @@ import {
   PreGeneratedChangeDetection
 } from 'angular2/change_detection';
 
-export var jitInjectables = [
+export var JIT_CHANGEDETECTION_BINDINGS = [
   bind(ChangeDetection).toClass(JitChangeDetection)
 ];
 
-export var dynamicInjectables = [
+export var DYNAMIC_CHANGEDETECTION_BINDINGS = [
   bind(ChangeDetection).toClass(DynamicChangeDetection)
 ];
 
-export var preGeneratedInjectables = [
+export var PREGENERATED_CHANGEDETECTION_BINDINGS = [
   bind(ChangeDetection).toClass(PreGeneratedChangeDetection)
 ];
 
-export var bestChangeDetectionInjectables = [
-  PreGeneratedChangeDetection.isSupported() ? preGeneratedInjectables :
-  JitChangeDetection.isSupported() ? jitInjectables : dynamicInjectables
+export var BEST_CHANGEDETECTION_BINDINGS = [
+  PreGeneratedChangeDetection.isSupported() ? PREGENERATED_CHANGEDETECTION_BINDINGS :
+  JitChangeDetection.isSupported() ? JIT_CHANGEDETECTION_BINDINGS : DYNAMIC_CHANGEDETECTION_BINDINGS
 ];
