87abc39a53ddf38a46e615020955cee5828249c9
gdi2290 <github@gdi2290.com>
2015-08-29 13:57:40 -0700

    refactor(todo_api): more in-memory into db file

---
+++ examples/server/db.js
@@ -0,0 +1,7 @@
+// your DB
+exports.todos = [
+  { value: 'finish example', created_at: new Date() },
+  { value: 'add tests',      created_at: new Date() },
+  { value: 'include development environment', created_at: new Date() },
+  { value: 'include production environment',  created_at: new Date() }
+];
--- examples/server/todo_api.js
+++ examples/server/todo_api.js
@@ -4,14 +4,7 @@ var util = require('util');
 // Express
 var express = require('express');
 
-// your API
-var count = 0;
-var todos = [
-  { value: 'finish example', created_at: new Date() },
-  { value: 'add tests',      created_at: new Date() },
-  { value: 'include development environment', created_at: new Date() },
-  { value: 'include production environment',  created_at: new Date() }
-];
+var db = require('./db');
 
 module.exports = function() {
   var router = express.Router();
@@ -20,13 +13,13 @@ module.exports = function() {
   router.route('/todos')
     .get(function(req, res) {
       console.log('GET');
-      res.json(todos);
+      res.json(db.todos);
     })
     .post(function(req, res) {
       console.log('POST', util.inspect(req.body, {colors: true}));
       var todo = req.body;
       if (todo) {
-        todos.push(todo);
+        db.todos.push(todo);
         res.json(todo);
       } else {
         res.end();
@@ -36,8 +29,8 @@ module.exports = function() {
   router.param('todo_id', function(req, res, next, todo_id) {
     var id = Number(req.params.todo_id);
     try {
-      var todo = todos[id];
-      req.todo = todos[id];
+      var todo = db.todos[id];
+      req.todo = db.todos[id];
       next();
     } catch(e) {
       next(new Error('failed to load todo'));
@@ -51,14 +44,14 @@ module.exports = function() {
     })
     .put(function(req, res) {
       console.log('PUT', util.inspect(req.body, {colors: true}));
-      var index = todos.indexOf(req.todo);
-      var todo = todos[index] = req.body;
+      var index = db.todos.indexOf(req.todo);
+      var todo = db.todos[index] = req.body;
       res.json(todo);
     })
     .delete(function(req, res) {
       console.log('DELETE');
-      var index = todos.indexOf(req.todo);
-      todos.splice(index, 1);
+      var index = db.todos.indexOf(req.todo);
+      db.todos.splice(index, 1);
       res.json(req.todo);
     });
 
