32a47ddd6ec36890a30b9d4715f03acd73a1d3dd
gdi2290 <github@gdi2290.com>
2015-08-31 22:00:29 -0700

    refactor(autofocus): use renderer

--- examples/app/directives/Autofocus.ts
+++ examples/app/directives/Autofocus.ts
@@ -1,14 +1,14 @@
 /// <reference path="../../typings/_custom.d.ts" />
 import {Directive, ElementRef} from 'angular2/angular2';
+import {Renderer} from 'angular2/render';
+
 // Simple example directive that fixes autofocus problem with multiple views
 @Directive({
   selector: '[autofocus]' // using [ ] means selecting attributes
 })
 export class Autofocus {
-  constructor(public el: ElementRef) {
+  constructor(el: ElementRef, renderer: Renderer) {
     // autofocus fix for multiple views
-    if (this.el.nativeElement.focus) {
-      this.el.nativeElement.focus();
-    }
+    renderer.invokeElementMethod(el, 'focus', []);
   }
 }
