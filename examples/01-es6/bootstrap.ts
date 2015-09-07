97553100c28f1a1f30f8ec48a9b0b6f19c65b9af
gdi2290 <github@gdi2290.com>
2015-09-07 14:37:42 -0700

    feat(01): include arrow functions

--- examples/01-es6/bootstrap.ts
+++ examples/01-es6/bootstrap.ts
@@ -64,10 +64,89 @@ amazing_people.push('Brendan Eich');
 
 console.log(amazing_people);
 
+amazing_people = ['Blake Embrey', 'TJ Holowaychuk']; // shouldn't happen
 
-amazing_people = ['Blake Embrey', 'TJ Holowaychuk']
-
+console.log('ref change', amazing_people); // warning happens
 
 console.log('-/const-\n');
+
+console.log('---------------------------------------------');
+console.log('---------------Arrow Functions---------------');
+
+
+
+var object = {
+  collection: ['PatrickJS', 'Lukas', 'Jeff', 'Dan'],
+  value: 'print some value:',
+  method: function() {
+    console.log(this.value, 'method');
+    this.collection.forEach(function(item) {
+      console.log(this.value, item);
+    });
+  }
+};
+
+object.method();
+
+
+var object2 = {
+  collection: ['PatrickJS', 'Lukas', 'Jeff', 'Dan'],
+  value: 'print some value:',
+  method: function() {
+    console.log(this.value, 'method');
+    this.collection.forEach((item) => {
+      console.log(this.value, item);
+    });
+  }
+};
+
+
+object2.method();
+
+/*
+ * what's happening is the context of when the function was created is preserved in the function
+
+var object2 = {
+  collection: ['PatrickJS', 'Lukas', 'Jeff', 'Dan'],
+  value: 'print some value:',
+  method: function() {
+    var _self = this;
+    console.log(this.value, 'method');
+    this.collection.forEach(function(item) {
+      console.log(_self.value, item);
+    });
+  }
+};
+
+ * here's another way to write this in ES5
+
+var object2 = {
+  collection: ['PatrickJS', 'Lukas', 'Jeff', 'Dan'],
+  value: 'print some value:',
+  method: function() {
+    console.log(this.value, 'method');
+    this.collection.forEach(function(item) {
+      console.log(this.value, item);
+    }.bind(this));
+  }
+};
+ */
+
+
+function callingBack(callback) {
+  callback();
+}
+
+console.log('arrow callback pattern');
+try {
+
+  callingBack(object2.method); // this doesn't work and throws an error
+
+} catch(e) {
+  console.error('arrow callback error:\n', e);
+}
+
+// very common pattern in order to preserve the context
+callingBack(() => object2.method());
 
 console.log('---------------------------------------------');
