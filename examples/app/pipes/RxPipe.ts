aed4e4edd0f6e58fcd8383ef287c03a132a37266
gdi2290 <github@gdi2290.com>
2015-09-10 13:38:27 -0700

    refactor(RxPIpe): update to use rx strategy

--- examples/app/pipes/RxPipe.ts
+++ examples/app/pipes/RxPipe.ts
@@ -1,35 +1,40 @@
 /// <reference path="../../typings/_custom.d.ts" />
 import {Pipe} from 'angular2/angular2';
-import {ObservablePipe} from 'angular2/pipes';
+import {AsyncPipe} from "angular2/pipes";
 import * as Rx from 'rx';
 
 export function isObservable(obs) {
   return obs && typeof obs.subscribe === 'function';
 }
 
+export class RxStrategy {
+  createSubscription(async, updateLatestValue) {
+    return async.subscribe(updateLatestValue, e => { throw e; });
+  }
+  dispose(subscription) {
+    subscription.dispose();
+  }
+  onDestroy(subscription) {
+    subscription.dispose();
+  }
+}
+
+export const RX_STRATEGY: RxStrategy = new RxStrategy();
+
 @Pipe({
   name: 'rx'
 })
-export class RxPipe extends ObservablePipe {
-  _subscription: any;
-  _observable: any;
-  constructor(ref) { super(ref); }
-
-  supports(obs) { return isObservable(obs); }
-
-  _subscribe(obs) {
-    this._observable = obs;
-    this._subscription = obs.subscribe(
-      value => this._updateLatestValue(value),
-      e => { throw e; }
-    );
-  }
-  transform(value: any, args?: List<any>): any {
-    return super.transform(value, args);
+export class RxPipe extends AsyncPipe {
+  constructor(public _ref: ChangeDetectorRef) {
+    super(_ref);
   }
 
-  onDestroy(): void {
-    return super.onDestroy();
+  supports(obs): boolean {
+    return isObservable(obs);
+  }
+
+  _selectStrategy(obj: Rx.Observable<any>): any {
+    return RX_STRATEGY;
   }
 
 }
