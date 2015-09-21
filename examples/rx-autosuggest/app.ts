27e68ba5aef56efbee08d149ec4af51e80e01a2a
gdi2290 <github@gdi2290.com>
2015-09-21 15:41:30 -0700

    refactor(examples): rx-autosuggest

---
+++ examples/rx-autosuggest/app.ts
@@ -0,0 +1,28 @@
+/// <reference path="../typings/_custom.d.ts" />
+
+// Angular 2
+import {Component, View} from 'angular2/angular2';
+
+import {SearchGithub} from './components/search-github';
+
+/*
+ * App Component
+ * our top level component that holds all of our components
+ */
+@Component({
+  selector: 'app'
+})
+@View({
+  directives: [ SearchGithub ],
+  template: `
+  <main>
+    <search-github></search-github>
+  </main>
+  `
+})
+class App {
+  constructor() {
+
+  }
+
+}
---
+++ examples/rx-autosuggest/directives/ac-autosuggest.ts
@@ -0,0 +1,54 @@
+/// <reference path="../../typings/_custom.d.ts" />
+
+// Angular 2
+import {Directive, View, EventEmitter, ElementRef} from 'angular2/angular2';
+
+// RxJs
+import * as Rx from 'rx';
+
+import {GithubService} from '../services/GithubService';
+
+@Directive({
+  selector: 'input[type=text][ac-autosuggest-github]',
+  events:   [ 'results', 'loading' ]
+})
+export class AcAutosuggestGithub {
+  results: EventEmitter = new EventEmitter();
+  loading: EventEmitter = new EventEmitter();
+
+  constructor(private el: ElementRef, public github: GithubService) {
+
+  }
+
+  // Lifecycle hook
+  onInit() {
+
+    (<any>Rx).Observable.fromEvent(this.el.nativeElement, 'input')
+      .map(e => e.target.value)                  // Project the text from the input
+      .filter(text => text.length > 2)           // Only if the text is longer than 2 characters
+      .debounce(250)                             // Pause for 250ms
+      .distinctUntilChanged()                    // Only if the value has changed
+      .do(() => this.loading.next(true))
+      .flatMapLatest(query => this.github.search(query)) // send query to search service
+      .do(() => this.loading.next(false))
+      // here is the real action
+      .subscribe(
+        // onNext
+        repos => {
+          // fire "results" event
+          // the Search component is the listener
+          this.results.next(repos);
+        },
+        // onError
+        err => {
+          console.log(err);;
+          this.results.next(['ERROR, see console']);
+        },
+        // onComplete
+        () => {
+          console.log('complete');
+        }
+      )//subscribe
+  }
+
+}
