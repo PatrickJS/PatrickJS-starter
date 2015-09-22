b93edf8bfa65715af1ac19b71f787cda01a7084f
gdi2290 <github@gdi2290.com>
2015-09-21 21:09:00 -0700

    refactor(bindings):  change detection

--- src/bindings/change_detection_bindings.ts
+++ src/bindings/change_detection_bindings.ts
@@ -7,19 +7,22 @@ import {
   PreGeneratedChangeDetection
 } from 'angular2/change_detection';
 
-export var JIT_CHANGEDETECTION_BINDINGS = [
+export const JIT_CHANGEDETECTION_BINDINGS = [
   bind(ChangeDetection).toClass(JitChangeDetection)
 ];
 
-export var DYNAMIC_CHANGEDETECTION_BINDINGS = [
+export const DYNAMIC_CHANGEDETECTION_BINDINGS = [
   bind(ChangeDetection).toClass(DynamicChangeDetection)
 ];
 
-export var PREGENERATED_CHANGEDETECTION_BINDINGS = [
+export const PREGENERATED_CHANGEDETECTION_BINDINGS = [
   bind(ChangeDetection).toClass(PreGeneratedChangeDetection)
 ];
 
-export var BEST_CHANGEDETECTION_BINDINGS = [
-  PreGeneratedChangeDetection.isSupported() ? PREGENERATED_CHANGEDETECTION_BINDINGS :
-  JitChangeDetection.isSupported() ? JIT_CHANGEDETECTION_BINDINGS : DYNAMIC_CHANGEDETECTION_BINDINGS
+export const BEST_CHANGEDETECTION_BINDINGS = [
+  PreGeneratedChangeDetection.isSupported() ?
+      PREGENERATED_CHANGEDETECTION_BINDINGS :
+             JitChangeDetection.isSupported() ?
+                 JIT_CHANGEDETECTION_BINDINGS :
+               DYNAMIC_CHANGEDETECTION_BINDINGS
 ];
