82c0e6c90012c77ef60d732be9250155199d6ab9
Jim Cummins <jcummins@fisdap.net>
2015-10-26 21:49:47 -0500

    chore(tests): use ts-loader
    Closes #91 closes #110

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
     },
 
     webpack: {
-
+      
       resolve: {
         root: __dirname,
         extensions: ['','.ts','.js','.json'],
@@ -45,7 +45,7 @@ module.exports = function(config) {
       devtool: 'inline-source-map',
       module: {
         loaders: [
-          { test: /\.ts$/,   loader: 'typescript-simple?ignoreWarnings[]=2345', exclude: [
+          { test: /\.tsx?$/,   loader: 'ts-loader', exclude: [
               /web_modules/,
               /node_modules/
             ]
