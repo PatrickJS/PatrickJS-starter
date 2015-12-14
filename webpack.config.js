f64f77ed664bd2010a4e5dc51db7ac082bdd9957
gdi2290 <github@gdi2290.com>
2015-12-13 20:53:44 -0800

    refactor(webpack.conf): reorg

--- webpack.config.js
+++ webpack.config.js
@@ -14,51 +14,54 @@ var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
  * Config
  */
 module.exports = {
-  devtool: 'source-map', // for faster builds use 'eval'
+  // for faster builds use 'eval'
+  devtool: 'source-map',
   debug: true,
-
-  // our Webpack Development Server config
-  devServer: {
-    historyApiFallback: true,
-    contentBase: 'src/public',
-    publicPath: '/__build__'
-  },
 
   entry: {
     'vendor': './src/vendor.ts',
+    'app': './src/bootstrap.ts' // our angular app
   },
 
   // Config for our build files
   output: {
     path: root('__build__'),
     filename: '[name].js',
-    sourceMapFilename: '[name].js.map',
+    sourceMapFilename: '[name].map',
     chunkFilename: '[id].chunk.js'
   },
 
   resolve: {
+    // ensure loader extensions match
     extensions: ['','.ts','.js','.json', '.css', '.html']
   },
 
   module: {
     preLoaders: [ { test: /\.ts$/, loader: 'tslint-loader' } ],
     loaders: [
-      // Support for *.json files.
-      { test: /\.json$/,  loader: 'json' },
-
-      // Support for CSS as raw text
-      { test: /\.css$/,   loader: 'raw' },
-
-      // support for .html as raw text
-      { test: /\.html$/,  loader: 'raw' },
-
       // Support for .ts files.
       {
         test: /\.ts$/,
-        loader: 'ts',
-        query: { 'ignoreDiagnostics': [ 2403 ] }, // 2403 -> Subsequent variable declarations
+        loader: 'ts-loader',
+        query: {
+          'ignoreDiagnostics': [
+            2403, // 2403 -> Subsequent variable declarations
+            2300, // 2300 Duplicate identifier
+            2374, // 2374 -> Duplicate number index signature
+            2375  // 2375 -> Duplicate string index signature
+          ]
+        },
         exclude: [ /\.spec\.ts$/, /\.e2e\.ts$/, /node_modules/ ]
-      }
+      },
+
+      // Support for *.json files.
+      { test: /\.json$/,  loader: 'json-loader' },
+
+      // Support for CSS as raw text
+      { test: /\.css$/,   loader: 'raw-loader' },
+
+      // support for .html as raw text
+      { test: /\.html$/,  loader: 'raw-loader' }
     ]
   },
 
@@ -66,6 +69,19 @@ module.exports = {
     new CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js', minChunks: Infinity }),
     new CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app'] })
   ],
+
+  // Other module loader config
+  tslint: {
+    emitErrors: false,
+    failOnHint: false
+  },
+  // our Webpack Development Server config
+  devServer: {
+    historyApiFallback: true,
+    contentBase: 'src/public',
+    publicPath: '/__build__'
+  }
+
 };
 
 // Helper functions
