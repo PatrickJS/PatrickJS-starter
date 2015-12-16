5c69325df0c2a6c3461762327e386b723a1a0e66
gdi2290 <github@gdi2290.com>
2015-12-15 22:38:02 -0800

    feat: include long-stack-trace-zone for zone.js

--- src/vendor.ts
+++ src/vendor.ts
@@ -3,6 +3,7 @@ import 'es6-promise';
 import 'es6-shim';
 import 'reflect-metadata';
 import 'zone.js/dist/zone-microtask';
+import 'zone.js/dist/long-stack-trace-zone';
 
 // Angular 2
 import 'angular2/platform/browser';
