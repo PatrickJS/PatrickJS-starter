9df9508ec9bd380339213eeb704eaef82f016a58
jennyraj <jennyraj@users.noreply.github.com>
2015-12-19 10:47:20 -0500

    Revert "merge this"

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,9 +1,7 @@
 // Polyfills
-import 'es6-promise';
-import 'es6-shim';
+import 'core-js';
 import 'reflect-metadata';
-import 'zone.js/dist/zone-microtask';
-import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js';
 
 // Angular 2
 import 'angular2/platform/browser';
