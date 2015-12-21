f52d256cd39eb258f657e399a43c0d0c83504bf2
gdi2290 <github@gdi2290.com>
2015-12-21 09:46:49 -0800

    test(home): increase coverage

--- src/app/home/home.spec.ts
+++ src/app/home/home.spec.ts
@@ -2,6 +2,7 @@ import {
   it,
   inject,
   injectAsync,
+  describe,
   beforeEachProviders,
   TestComponentBuilder
 } from 'angular2/testing';
@@ -28,8 +29,20 @@ describe('Home', () => {
       deps: [MockBackend, BaseRequestOptions]})
   ]);
 
-  it('should have a title', inject([ Home ], (app) => {
-    expect(app.title.value).toEqual('Angular 2');
+  it('should have a title', inject([ Home ], (home) => {
+    expect(home.title.value).toEqual('Angular 2');
+  }));
+
+  it('should have a http', inject([ Home ], (home) => {
+    expect(!!home.http).toEqual(true);
+  }));
+
+  it('should log ngOnInit', inject([ Home ], (home) => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    home.ngOnInit();
+    expect(console.log).toHaveBeenCalled();
   }));
 
 });
