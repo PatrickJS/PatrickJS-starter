ec3fa0db64daad80fc9094befa7207e87aeb6e09
gdi2290 <github@gdi2290.com>
2016-01-02 18:49:52 -0800

    chore(karma): use external webpack

--- karma.conf.js
+++ karma.conf.js
@@ -3,36 +3,27 @@ var path = require('path');
 
 module.exports = function(config) {
   var testWebpackConfig = require('./webpack.test.config.js');
-  var _config = {
+  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // base path that will be used to resolve all patterns (e.g. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
+    // list of files to exclude
+    exclude: [ ],
 
     // list of files / patterns to load in the browser
-    files: [
-      // we are building the test environment in ./spec-bundle.js
-      { pattern: 'spec-bundle.js', watched: false }
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
+    // we are building the test environment in ./spec-bundle.js
+    files: [ { pattern: 'spec-bundle.js', watched: false } ],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'spec-bundle.js': ['webpack', 'sourcemap']
-      // 'test/**/*.spec.ts': ['webpack', 'sourcemap']
-    },
+    preprocessors: { 'spec-bundle.js': ['webpack', 'sourcemap'] },
 
+    // Webpack Config at ./webpack.test.config.js
     webpack: testWebpackConfig,
 
     coverageReporter: {
@@ -43,44 +34,34 @@ module.exports = function(config) {
       ],
     },
 
-    webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    },
-
+    // Webpack please don't spam the console when running in karma!
+    webpackServer: { noInfo: true },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [ 'progress', 'coverage' ],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
+    browsers: [ 'PhantomJS' ],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
-  };
-
-  config.set(_config);
+  });
 
 };
