fe2a08d9046724300d8e9508eba954f2e980f67d
gdi2290 <github@gdi2290.com>
2016-01-02 18:46:23 -0800

    refactor(vendor): env flags

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,14 +1,30 @@
 // Polyfills
-import './polyfills';
+import 'es6-shim';
+// (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
+import 'es6-promise';
+import 'zone.js/lib/browser/zone-microtask';
 
+if ('production' !== process.env.ENV) {
+  // Reflect Polyfill
+  require('es7-reflect-metadata/dist/browser');
+  Error['stackTraceLimit'] = Infinity;
+  Zone['longStackTraceZone'] = require('zone.js/lib/zones/long-stack-trace.js');
+}
+
+if ('production' === process.env.ENV) {
+  // Reflect with es7-reflect-metadata/reflect-metadata is added
+  // by webpack.prod.config ProvidePlugin
+  let ngCore = require('angular2/core');
+  ngCore.enableProdMode();
+}
 // Angular 2
 import 'angular2/platform/browser';
 import 'angular2/platform/common_dom';
-import 'angular2/core';
 import 'angular2/router';
 import 'angular2/http';
+import 'angular2/core';
 
 // RxJS
 import 'rxjs';
 
-// Other vendors for example jQuery or Lodash
+// Other vendors for example jQuery, Lodash, angular2-jwt
