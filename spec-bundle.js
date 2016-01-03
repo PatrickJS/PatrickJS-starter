5639696b9c89a2a04cb0920360423c2d0db07094
gdi2290 <github@gdi2290.com>
2016-01-02 18:50:59 -0800

    refactor(spec-bundle): use polyfill in config

--- spec-bundle.js
+++ spec-bundle.js
@@ -13,20 +13,17 @@ require('phantomjs-polyfill');
 require('es6-promise');
 require('es6-shim');
 require('es7-reflect-metadata/dist/browser');
+
 require('zone.js/lib/browser/zone-microtask.js');
 require('zone.js/lib/browser/long-stack-trace-zone.js');
-require('zone.js/lib/browser/jasmine-patch.js');
-// these are global EmitHelpers used by compiled typescript
-globalPolyfills()
-
-require('angular2/testing');
+require('zone.js/dist/jasmine-patch.js');
 
 /*
   Ok, this is kinda crazy. We can use the the context method on
   require that webpack created in order to tell webpack
   what files we actually want to require or import.
   Below, context will be an function/object with file names as keys.
-  using that regex we are saying look in client/app and find
+  using that regex we are saying look in ./src/app and ./test then find
   any file that ends with spec.js and get its path. By passing in true
   we say do this recursively
 */
@@ -44,39 +41,3 @@ testContext.keys().forEach(testContext);
 var domAdapter = require('angular2/src/platform/browser/browser_adapter');
 domAdapter.BrowserDomAdapter.makeCurrent();
 
-
-
-
-// these are helpers that typescript uses
-// I manually added them by opting out of EmitHelpers by noEmitHelpers: false
-function globalPolyfills(){
-  global.__extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    var __ = function() { this.constructor = d; };
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-  };
-
-  global.__decorate = global.Reflect.decorate;
-  global.__metadata = global.Reflect.metadata;
-
-  global.__param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); };
-  };
-
-  global.__awaiter = (this && this.__awaiter) ||
-    function (thisArg, _arguments, Promise, generator) {
-      return new Promise(function (resolve, reject) {
-        generator = generator.call(thisArg, _arguments);
-        function cast(value) {
-          return value instanceof Promise && value.constructor === Promise ?
-          value : new Promise(function (resolve) { resolve(value); }); }
-        function onfulfill(value) { try { step('next', value); } catch (e) { reject(e); } }
-        function onreject(value) { try { step('throw', value); } catch (e) { reject(e); } }
-        function step(verb, value) {
-          var result = generator[verb](value);
-          result.done ? resolve(result.value) : cast(result.value).then(onfulfill, onreject);
-        }
-        step('next', void 0);
-      });
-    };
-}
