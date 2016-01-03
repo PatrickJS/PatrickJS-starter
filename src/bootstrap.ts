c9e5e01f00c0ad810b87f7940d1e2893df45d1d4
gdi2290 <github@gdi2290.com>
2016-01-02 18:51:11 -0800

    feat(bootstrap): correctly use env

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,33 +1,24 @@
 /*
  * Providers provided by Angular
  */
-import {bootstrap} from 'angular2/platform/browser';
+import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
-// include for development builds
-import {ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/common_dom';
-// include for production builds
-// import {enableProdMode} from 'angular2/core';
 
 /*
  * App Component
  * our top level component that holds all of our components
  */
 import {App} from './app/app';
-
 /*
  * Bootstrap our Angular app with a top level component `App` and inject
  * our Services and Providers into Angular's dependency injection
  */
-// enableProdMode() // include for production builds
-function main() {
-  return bootstrap(App, [
-    // These are dependencies of our App
-    HTTP_PROVIDERS,
-    ROUTER_PROVIDERS,
-    ELEMENT_PROBE_PROVIDERS // remove in production
+document.addEventListener('DOMContentLoaded', function main() {
+  bootstrap(App, [
+    ...HTTP_PROVIDERS,
+    ...ROUTER_PROVIDERS,
+    ...('production' === process.env.ENV ? [] : ELEMENT_PROBE_PROVIDERS)
   ])
   .catch(err => console.error(err));
-}
-
-document.addEventListener('DOMContentLoaded', main);
+});
