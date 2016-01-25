0439d93f8300dbff33acf769f93c6ecb30d74338
gdi2290 <github@gdi2290.com>
2016-01-24 17:39:46 -0800

    chore(karma): allow chrome in browser list

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,10 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ 'PhantomJS' ],
+    browsers: [
+      // 'Chrome',
+      'PhantomJS'
+    ],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
