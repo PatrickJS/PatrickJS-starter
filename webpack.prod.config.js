ce68f417032106c2325240071cbebcd91b72989d
gdi2290 <github@gdi2290.com>
2016-01-28 15:39:58 -0800

    docs(weboack.prod): document problem with beta.2

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -157,8 +157,8 @@ module.exports = {
       // comments: true,//debug
 
       beautify: false,//prod
-      // disable mangling because of a bug in angular2 beta.1
-      // TODO(mastertinner): enable mangling as soon as angular2 beta.2 is out
+      // disable mangling because of a bug in angular2 beta.1 and beta.2
+      // TODO(mastertinner): enable mangling as soon as angular2 beta.3 is out
       // mangle: { screw_ie8 : true },//prod
       mangle: false,
       compress : { screw_ie8 : true},//prod
