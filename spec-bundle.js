617876598d7e90f8890c111f93ba1cf2bda6f134
gdi2290 <github@gdi2290.com>
2016-02-03 18:50:51 -0800

    chore(spec-bundle): fix zone paths

--- spec-bundle.js
+++ spec-bundle.js
@@ -14,8 +14,8 @@ require('es6-promise');
 require('es6-shim');
 require('es7-reflect-metadata/dist/browser');
 
-require('zone.js/lib/browser/zone-microtask.js');
-require('zone.js/lib/browser/long-stack-trace-zone.js');
+require('zone.js/dist/zone-microtask.js');
+require('zone.js/dist/long-stack-trace-zone.js');
 require('zone.js/dist/jasmine-patch.js');
 
 
