0d25f165b01c915f79a72388d3f1b0589e0aba5c
dantedubon <dante.dubon@gmail.com>
2016-02-09 17:35:47 -0600

    configuring karma. Failing to exclude a single file

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,94 @@
 // @AngularClass
 var path = require('path');
 
-module.exports = function(config) {
-  var testWebpackConfig = require('./webpack.test.config.js');
-  config.set({
+module.exports = function (config) {
+    var testWebpackConfig = require('./webpack.test.config.js');
+    config.set({
 
-    // base path that will be used to resolve all patterns (e.g. files, exclude)
-    basePath: '',
+        // base path that will be used to resolve all patterns (e.g. files, exclude)
+        basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
-    // list of files to exclude
-    exclude: [ ],
+        // list of files to exclude
+        exclude: [],
 
-    // list of files / patterns to load in the browser
-    // we are building the test environment in ./spec-bundle.js
-    files: [ { pattern: 'spec-bundle.js', watched: false } ],
+        // list of files / patterns to load in the browser
+        // we are building the test environment in ./spec-bundle.js
+        files: [{ pattern: 'spec-bundle.js', watched: false }],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { 'spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: { 'spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
 
-    // Webpack Config at ./webpack.test.config.js
-    webpack: testWebpackConfig,
+        // Webpack Config at ./webpack.test.config.js
+        webpack: testWebpackConfig,
 
-    coverageReporter: {
-      dir : 'coverage/',
-      reporters: [
-        { type: 'text-summary' },
-        { type: 'json' },
-        { type: 'html' }
-      ]
-    },
+        coverageReporter: {
+            dir: 'coverage/',
+            reporters: [
+                { type: 'text-summary' },
+                { type: 'json' },
+                { type: 'html' }
+            ],
+            check: {
+                global: {
+                    statements: 50,
+                    branches: 0,
+                    functions: 50,
+                    lines: 50,
+                    excludes: [
+                        'src/app/directives/router-active.js'
+                    ]
+                },
+                each: {
+                    statements: 50,
+                    branches: 50,
+                    functions: 50,
+                    lines: 50,
+                    excludes: [
+                        'src/app/directives/router-active.js'
+                    ],
+                    overrides: {
 
-    // Webpack please don't spam the console when running in karma!
-    webpackServer: { noInfo: true },
+                    }
+                }
+            }
+        },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'mocha', 'coverage' ],
+        // Webpack please don't spam the console when running in karma!
+        webpackServer: { noInfo: true },
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['mocha', 'coverage'],
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-      'Chrome',
-      'PhantomJS'
-    ],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: [
+            'Chrome',
+            'PhantomJS'
+        ],
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    });
 
 };
--- src/app/directives/router-active.ts
+++ src/app/directives/router-active.ts
@@ -1,13 +1,14 @@
+/* istanbul ignore next */
 import {Router} from 'angular2/router';
 import {isPresent} from 'angular2/src/facade/lang';
 import {
-  Directive,
-  Query,
-  QueryList,
-  Attribute,
-  ElementRef,
-  Renderer,
-  Optional
+Directive,
+Query,
+QueryList,
+Attribute,
+ElementRef,
+Renderer,
+Optional
 } from 'angular2/core';
 import {Instruction, RouterLink} from 'angular2/router';
 
@@ -21,39 +22,43 @@ import {Instruction, RouterLink} from 'angular2/router';
  * <li [routerActive]=" activeStringValue "><a [routerLink]=" ['/Home'] ">Home</a></li>
  * ```
  */
+
 @Directive({
-  selector: '[router-active], [routerActive]',
-  inputs: ['routerActive']
+    selector: '[router-active], [routerActive]',
+    inputs: ['routerActive']
 })
+
+
 export class RouterActive {
-  routerActive: string = null;
-  routerActiveAttr: string = 'active';
+    routerActive: string = null;
+    routerActiveAttr: string = 'active';
 
-  constructor(
-    public router: Router,
-    public element: ElementRef,
-    public renderer: Renderer,
-    @Query(RouterLink) public routerLink: QueryList<RouterLink>,
-    @Optional() @Attribute('router-active') routerActiveAttr?: string) {
+    constructor(
+        public router: Router,
+        public element: ElementRef,
+        public renderer: Renderer,
+        @Query(RouterLink) public routerLink: QueryList<RouterLink>,
+        @Optional() @Attribute('router-active') routerActiveAttr?: string) {
 
-      this.routerActiveAttr = this._defaultAttrValue(routerActiveAttr);
-  }
+        this.routerActiveAttr = this._defaultAttrValue(routerActiveAttr);
+    }
 
-  ngOnInit() {
-    this.router.subscribe(() => {
-      if (this.routerLink.first) {
-        let active = this.routerLink.first.isRouteActive;
-        this.renderer.setElementClass(this.element.nativeElement, this._attrOrProp(), active);
-      }
-    });
+    ngOnInit() {
+        this.router.subscribe(() => {
+            if (this.routerLink.first) {
+                let active = this.routerLink.first.isRouteActive;
+                this.renderer.setElementClass(this.element.nativeElement,
+                    this._attrOrProp(), active);
+            }
+        });
 
-  }
+    }
 
-  private _defaultAttrValue(attr?: string) {
-    return this.routerActiveAttr = attr || this.routerActiveAttr;
-  }
+    private _defaultAttrValue(attr?: string) {
+        return this.routerActiveAttr = attr || this.routerActiveAttr;
+    }
 
-  private _attrOrProp() {
-    return isPresent(this.routerActive) ? this.routerActive : this.routerActiveAttr;
-  }
+    private _attrOrProp() {
+        return isPresent(this.routerActive) ? this.routerActive : this.routerActiveAttr;
+    }
 }
