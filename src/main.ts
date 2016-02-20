13ff8273e2777dcf143e669bf82c4d6d90bd32c7
Daniel Nehring <nehrdani@gmail.com>
2016-02-21 00:58:46 +0100

    updated the mian.ts file to use hot module replacing

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ import {App} from './app/app';
  * Bootstrap our Angular app with a top level component `App` and inject
  * our Services and Providers into Angular's dependency injection
  */
+
 document.addEventListener('DOMContentLoaded', function main() {
   bootstrap(App, [
     ...ENV_PROVIDERS,
@@ -34,6 +35,33 @@ document.addEventListener('DOMContentLoaded', function main() {
   .catch(err => console.error(err));
 
 });
+
+
+/*
+ * Modified for using hot module reload
+ */
+
+// typescript lint error 'Cannot find name "module"' fix
+declare let module: any;
+
+// activate hot module reload
+if (module.hot) {
+
+  // bootstrap must not be called after DOMContentLoaded,
+  // otherwise it cannot be rerenderd after module replacement
+  //
+  // for testing try to comment the bootstrap function,
+  // open the dev tools and you'll see the reloader is replacing the module but cannot rerender it
+  bootstrap(App, [
+      ...ENV_PROVIDERS,
+      ...HTTP_PROVIDERS,
+      ...ROUTER_PROVIDERS,
+      provide(LocationStrategy, { useClass: HashLocationStrategy })
+    ])
+    .catch(err => console.error(err));
+
+  module.hot.accept();
+}
 
 // For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
 // Also see custom_typings.d.ts as you also need to do `typings install x` where `x` is your module
