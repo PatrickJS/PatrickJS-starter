4c6259cc59f0b72d40826d583acaa737c507a7e0
gdi2290 <github@gdi2290.com>
2016-02-25 15:28:37 -0800

    style(karma.conf): remove path

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,8 @@
 // @AngularClass
-var path = require('path');
 
 module.exports = function(config) {
   var testWebpackConfig = require('./webpack.test.config.js');
+
   config.set({
 
     // base path that will be used to resolve all patterns (e.g. files, exclude)
