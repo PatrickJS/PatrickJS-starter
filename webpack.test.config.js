5af1e9a04e7d4ef85ffa6a069f09d16e6e2c758e
gdi2290 <github@gdi2290.com>
2016-03-11 23:36:37 -0800

    chore(webpack.test): remove excludes

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -40,11 +40,11 @@ module.exports = {
             "removeComments": true,
           }
         },
-        exclude: [ /\.e2e\.ts$/, helpers.root('node_modules') ]
+        exclude: [ /\.e2e\.ts$/ ]
       },
-      { test: /\.json$/, loader: 'json-loader', exclude: [ helpers.root('src/index.html'), helpers.root('node_modules') ] },
-      { test: /\.html$/, loader: 'raw-loader', exclude: [ helpers.root('src/index.html'), helpers.root('node_modules') ] },
-      { test: /\.css$/,  loader: 'raw-loader', exclude: [ helpers.root('src/index.html'), helpers.root('node_modules') ] }
+      { test: /\.json$/, loader: 'json-loader', exclude: [ helpers.root('src/index.html') ] },
+      { test: /\.html$/, loader: 'raw-loader', exclude: [ helpers.root('src/index.html') ] },
+      { test: /\.css$/,  loader: 'raw-loader', exclude: [ helpers.root('src/index.html') ] }
     ],
     postLoaders: [
       // instrument only testing sources with Istanbul
