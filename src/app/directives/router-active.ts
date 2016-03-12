6472c3459bab5ed5723a8807e27a847f8a249504
gdi2290 <github@gdi2290.com>
2016-03-12 00:54:47 -0800

    fix: ng2lint

--- src/app/directives/router-active.ts
+++ src/app/directives/router-active.ts
@@ -7,7 +7,8 @@ import {
   Attribute,
   ElementRef,
   Renderer,
-  Optional
+  Optional,
+  Input
 } from 'angular2/core';
 import {Instruction, RouterLink} from 'angular2/router';
 
@@ -22,11 +23,10 @@ import {Instruction, RouterLink} from 'angular2/router';
  * ```
  */
 @Directive({
-  selector: '[router-active], [routerActive]',
-  inputs: ['routerActive']
+  selector: '[router-active], [routerActive]'
 })
 export class RouterActive {
-  routerActive: string = null;
+  @Input() routerActive: string = null;
   routerActiveAttr: string = 'active';
 
   constructor(
