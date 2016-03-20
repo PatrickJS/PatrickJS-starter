962ea9cec1c6edf3dcc2a667471c71359c304f6e
gdi2290 <github@gdi2290.com>
2016-03-20 04:01:15 -0700

    feat(platform): directives

---
+++ src/platform/browser/directives.ts
@@ -0,0 +1,31 @@
+import {provide, PLATFORM_DIRECTIVES} from 'angular2/core';
+
+// Angular 2 Router
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+
+// Angular 2 Material
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdRadioButton} from '@angular2-material/radio';
+import {MdSpinner} from '@angular2-material/progress-circle';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+
+// App/directives
+import {RouterActive} from '../app/directives/router-active';
+
+// application_directives: directives that are global through out the application
+export const APPLICATION_DIRECTIVES = [
+  ...ROUTER_DIRECTIVES,
+
+  ...MD_SIDENAV_DIRECTIVES,
+  ...[ MdToolbar, MdButton, MdCheckbox, MdRadioButton, MdSpinner ],
+  ...MD_CARD_DIRECTIVES,
+
+  RouterActive
+];
+
+export const DIRECTIVES = [
+  provide(PLATFORM_DIRECTIVES, {useValue: APPLICATION_DIRECTIVES, multi: true})
+];
