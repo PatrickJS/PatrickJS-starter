a7f979c344faf04bf1614e9884a54316b03ba6df
gdi2290 <github@gdi2290.com>
2016-03-20 12:38:49 -0700

    style(about): include more comments about async

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -27,14 +27,16 @@ export class About {
   }
   asyncDataWithWebpack() {
     // you can also async load mock data with 'es6-promise-loader'
-    var asyncMockData = require('es6-promise!assets/mock-data/mock-data.json');
+    // you would do this if you don't want the mock-data bundled
+    // remember that 'es6-promise-loader' is a promise
+    var asyncMockDataPromiseFactory = require('es6-promise!assets/mock-data/mock-data.json');
     setTimeout(() => {
 
-      let asyncData = asyncMockData();
-      asyncData.then(json => {
+      let asyncDataPromise = asyncMockDataPromiseFactory();
+      asyncDataPromise.then(json => {
         console.log('async mockData', json);
       });
-      
+
     });
   }
 
