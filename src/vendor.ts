cf1509bb77545515ecde86020f0ace42169ada17
gdi2290 <github@gdi2290.com>
2016-03-27 18:27:28 -0700

    chore(vendor): include more ng2 in vendor

--- src/vendor.ts
+++ src/vendor.ts
@@ -5,7 +5,9 @@
 
 // Angular 2
 import 'angular2/platform/browser';
+import 'angular2/platform/common_dom';
 import 'angular2/core';
+import 'angular2/common';
 import 'angular2/http';
 import 'angular2/router';
 
