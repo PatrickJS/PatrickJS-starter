2f8c81cd4abe332cc7ec69a24556984360e330e1
PatrickJS <github@gdi2290.com>
2016-04-12 11:09:35 -0700

    chore(home): typo

--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -8,7 +8,7 @@ describe('App', () => {
 
   it('should have a title', () => {
     let subject = browser.getTitle();
-    let result  = 'Angular2 Webpack Starter by @gdi2990 from @AngularClass';
+    let result  = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
     expect(subject).toEqual(result);
   });
 
