2b299484c6b499dd818497b2a9582728b6c7ada0
PatrickJS <github@gdi2290.com>
2016-04-14 22:02:42 -0700

    feat(webpack.config): switch env

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,17 @@
  */
 
 // Look in ./config folder for webpack.dev.js
-module.exports = require('./config/webpack.dev.js');
+switch (process.env.NODE_ENV) {
+  case 'prod':
+  case 'production':
+    module.exports = require('./config/webpack.prod');
+    break;
+  case 'test':
+  case 'testing':
+    module.exports = require('./config/webpack.test');
+    break;
+  case 'dev':
+  case 'development':
+  default:
+    module.exports = require('./config/webpack.dev');
+}
