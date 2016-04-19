81fc4375f31241312541c25528022af021f6e350
Martin Hochel <hochelmartin@gmail.com>
2016-04-19 05:23:27 +0200

    chore(tsc): don't emit helpers and use maintained polyfill for that instead (#520)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -11,6 +11,9 @@ import 'core-js/es6';
 import 'core-js/es7/reflect';
 require('zone.js/dist/zone');
 
+// Typescript emit helpers polyfill
+import 'ts-helpers';
+
 if ('production' === ENV) {
   // Production
 
