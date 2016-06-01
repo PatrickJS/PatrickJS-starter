45e5656707bce1f680dd73011f4218fb199866e0
Shlomi Assaf (shlassaf) <shlassaf@cisco.com>
2016-06-01 18:41:21 +0300

    chore: add empty line

--- config/helpers.js
+++ config/helpers.js
@@ -16,6 +16,7 @@ function hasProcessFlag(flag) {
 function isWebpackDevServer() {
   return process.argv[1] && !! (/webpack-dev-server$/.exec(process.argv[1]));
 }
+
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [ROOT].concat(args));
