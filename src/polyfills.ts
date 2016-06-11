b5181395bf9a2b8d2f5c198de5438f54b8e67f87
PatrickJS <github@gdi2290.com>
2016-06-11 14:06:43 -0700

    refactor(polyfills): use import for zonejs.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -9,7 +9,7 @@
 // Prefer CoreJS over the polyfills above
 import 'core-js/es6';
 import 'core-js/es7/reflect';
-require('zone.js/dist/zone');
+import 'zone.js/dist/zone';
 
 // Typescript emit helpers polyfill
 import 'ts-helpers';
