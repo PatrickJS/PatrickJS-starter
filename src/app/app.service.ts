0e74973f6b746f840837e210db574fd2821968a5
leonardlin <leonardlin@users.noreply.github.com>
2016-06-12 03:46:20 +0200

    fix(AppState): bool false value not returned
    this resolves the issue #673, that boolean properties with value 'false' are not returned.

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -23,7 +23,7 @@ export class AppState {
   get(prop?: any) {
     // use our state getter for the clone
     const state = this.state;
-    return state[prop] || state;
+    return state.hasOwnProperty(prop) ? state[prop] : state;
   }
 
   set(prop: string, value: any) {
