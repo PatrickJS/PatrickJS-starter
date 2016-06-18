da9f14f090aff49289fa659b268f97951cb15dc5
PatrickJS <github@gdi2290.com>
2016-06-18 16:58:06 -0700

    feat: use new @angular/form api

--- src/platform/browser-providers.ts
+++ src/platform/browser-providers.ts
@@ -3,18 +3,22 @@
  */
 
 // Angular 2
-import { FORM_PROVIDERS, HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 // Angular 2 Http
 import { HTTP_PROVIDERS } from '@angular/http';
 // Angular 2 Router
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
-
+// Angular 2 forms
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 /*
 * Application Providers/Directives/Pipes
 * providers/directives/pipes that only live in our browser environment
 */
 export const APPLICATION_PROVIDERS = [
-  ...FORM_PROVIDERS,
+  // new Angular 2 forms
+  disableDeprecatedForms(),
+  provideForms(),
+
   ...HTTP_PROVIDERS,
   ...ROUTER_PROVIDERS,
   { provide: LocationStrategy, useClass: HashLocationStrategy }
