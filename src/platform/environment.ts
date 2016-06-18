ce060c48234e654bd4bb58fddb8ec0eec70f68b7
PatrickJS <github@gdi2290.com>
2016-06-18 08:43:13 -0700

    fix(environment): correct compiler config

--- src/platform/environment.ts
+++ src/platform/environment.ts
@@ -5,7 +5,7 @@ import { enableProdMode as enableProdMode0 } from '@angular/core/src/facade/lang
 import { enableProdMode as enableProdMode1 } from '@angular/compiler/src/facade/lang';
 import { enableProdMode as enableProdMode2 } from '@angular/platform-browser/src/facade/lang';
 import { CompilerConfig } from '@angular/compiler';
-
+import { PLATFORM_DIRECTIVES, PLATFORM_PIPES } from '@angular/core';
 // Environment Providers
 let PROVIDERS = [
   // common env directives
@@ -19,14 +19,21 @@ if ('production' === ENV) {
 
   PROVIDERS = [
     ...PROVIDERS,
-    // custom providers in production
+    // rc2 workaround
     {
       provide: CompilerConfig,
-      useValue: new CompilerConfig({
-        genDebugInfo: false,
-        logBindingUpdate: false
-      })
-    }
+      useFactory: (platformDirectives: any[], platformPipes: any[]) => {
+        let compiler = new CompilerConfig({
+          genDebugInfo: true,
+          logBindingUpdate: true,
+          platformPipes,
+          platformDirectives
+        });
+        return compiler;
+      },
+      deps: [PLATFORM_DIRECTIVES, PLATFORM_PIPES]
+    },
+    // custom providers in production
   ];
 
 } else {
