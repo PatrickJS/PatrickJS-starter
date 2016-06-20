920fc3e53300111db949be46c758026c69fd393e
PatrickJS <github@gdi2290.com>
2016-06-20 12:42:59 -0700

    chore: create routes file with async routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Home } from './home';
+
+export const routes = [
+  { path: '',      component: Home },
+  { path: 'home',  component: Home },
+  // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
+  // make sure you match the component type string to the require in asyncRoutes
+  { path: 'about', component: 'About' }
+];
+
+
+export const asyncRoutes = {
+  'About': require('es6-promise!./about')
+};
