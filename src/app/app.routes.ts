59b46f27726c9cca316740446b09a6e0e4019a9a
PatrickJS <github@gdi2290.com>
2016-06-20 20:09:15 -0700

    feat: NoContent 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { RouterConfig } from '@angular/router';
 import { Home } from './home';
+import { NoContent } from './no-content';
 
 export const routes: RouterConfig = [
   { path: '',      component: Home },
   { path: 'home',  component: Home },
   // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
   // make sure you match the component type string to the require in asyncRoutes
-  { path: 'about', component: 'About' }
+  { path: 'about', component: 'About' },
+  { path: '**',    component: NoContent },
 ];
 
 
