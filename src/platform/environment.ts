a1c31f1a04db5d699b030469c9f2a0757cc6bd4f
PatrickJS <github@gdi2290.com>
2016-06-20 21:27:40 -0700

    refactor(environment): typo

--- src/platform/environment.ts
+++ src/platform/environment.ts
@@ -14,7 +14,7 @@ let PROVIDERS = [
 
 // Angular debug tools in the dev console
 // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
-let _decorateComponentRef = function idenity(value) { return value; };
+let _decorateComponentRef = function identity(value) { return value; };
 
 if ('production' === ENV) {
   // Production
