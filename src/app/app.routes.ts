0c461409b6db01dad5fba68807adad5a545b8c43
PatrickJS <github@gdi2290.com>
2016-06-21 21:11:33 -0700

    docs(app.routes): comments about routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,20 @@ import { NoContent } from './no-content';
 export const routes: RouterConfig = [
   { path: '',      component: Home },
   { path: 'home',  component: Home },
-  // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
   // make sure you match the component type string to the require in asyncRoutes
   { path: 'about', component: 'About' },
   { path: '**',    component: NoContent },
 ];
 
-// asyncRoutes is needed for our webpack-toolkit to allow us to resolve the component correctly
+// Async load a component using Webpack's require with es6-promise-loader and webpack `require`
+// asyncRoutes is needed for our @angularclass/webpack-toolkit that will allow us to resolve
+// the component correctly
 export const asyncRoutes: AsyncRoutes = {
   'About': require('es6-promise-loader!./about')
 };
 
+
+// Optimizations for initial loads
 // An array of callbacks to be invoked after bootstrap to prefetch async routes
 export const prefetchRouteCallbacks: Array<Es6PromiseLoader | Function> = [
   asyncRoutes['About'] // es6-promise-loader returns a function
