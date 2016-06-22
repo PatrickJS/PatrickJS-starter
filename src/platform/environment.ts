b05db926f3e29b7501fea77ebb445014af0dd32a
PatrickJS <github@gdi2290.com>
2016-06-21 18:19:26 -0700

    chore: update Angular 2.0.0-rc.3

--- src/platform/environment.ts
+++ src/platform/environment.ts
@@ -2,11 +2,7 @@
 // Angular 2
 // rc2 workaround
 import { enableDebugTools, disableDebugTools } from '@angular/platform-browser';
-import { enableProdMode as enableProdMode0 } from '@angular/core/src/facade/lang';
-import { enableProdMode as enableProdMode1 } from '@angular/compiler/src/facade/lang';
-import { enableProdMode as enableProdMode2 } from '@angular/platform-browser/src/facade/lang';
-import { CompilerConfig } from '@angular/compiler';
-import { PLATFORM_DIRECTIVES, PLATFORM_PIPES } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 // Environment Providers
 let PROVIDERS = [
   // common env directives
@@ -19,26 +15,10 @@ let _decorateComponentRef = function identity(value) { return value; };
 if ('production' === ENV) {
   // Production
   disableDebugTools();
-  enableProdMode0();
-  enableProdMode1();
-  enableProdMode2();
+  enableProdMode();
 
   PROVIDERS = [
     ...PROVIDERS,
-    // rc2 workaround
-    {
-      provide: CompilerConfig,
-      useFactory: (platformDirectives: any[], platformPipes: any[]) => {
-        let compiler = new CompilerConfig({
-          genDebugInfo: true,
-          logBindingUpdate: true,
-          platformPipes,
-          platformDirectives
-        });
-        return compiler;
-      },
-      deps: [PLATFORM_DIRECTIVES, PLATFORM_PIPES]
-    },
     // custom providers in production
   ];
 
