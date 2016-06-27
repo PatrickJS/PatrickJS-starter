3cad4cee85a25430f8395561158b7f221bf6a17a
PatrickJS <github@gdi2290.com>
2016-06-27 13:41:15 -0700

    chore: include async folder

---
+++ src/app/+detail/detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'detail',
+
+  template: `
+    <h1>Hello from Detail</h1>
+  `
+})
+export class Detail {
+
+}
---
+++ src/app/+detail/index.ts
@@ -0,0 +1,5 @@
+export { Detail } from './detail.component';
+export { Index } from './index.component';
+
+// Must be exported for WebpackAsyncRoute
+export * from './routes';
