55b69bedafffe4c5cc16dbd2e0b0a444f46a9aa9
PatrickJS <github@gdi2290.com>
2016-06-27 13:43:00 -0700

    chore: include +details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { WebpackAsyncRoute } from '@angularclass/webpack-toolkit';
 import { RouterConfig } from '@angular/router';
 import { Home } from './home';
 import { NoContent } from './no-content';
@@ -7,21 +8,28 @@ export const routes: RouterConfig = [
   { path: 'home',  component: Home },
   // make sure you match the component type string to the require in asyncRoutes
   { path: 'about', component: 'About' },
+  // async components with children routes must use WebpackAsyncRoute
+  { path: 'detail', component: 'Detail', canActivate: [ WebpackAsyncRoute ] },
   { path: '**',    component: NoContent },
 ];
 
 // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
 // asyncRoutes is needed for our @angularclass/webpack-toolkit that will allow us to resolve
 // the component correctly
+
 export const asyncRoutes: AsyncRoutes = {
-  'About': require('es6-promise-loader!./about')
+  // we have to use the alternative syntax for es6-promise-loader to grab the routes
+  'About': require('es6-promise-loader!./about'),
+  'Detail': require('es6-promise-loader!./+detail'),
 };
 
 
 // Optimizations for initial loads
 // An array of callbacks to be invoked after bootstrap to prefetch async routes
-export const prefetchRouteCallbacks: Array<Es6PromiseLoader | Function> = [
-  asyncRoutes['About'] // es6-promise-loader returns a function
+export const prefetchRouteCallbacks: Array<IdleCallbacks> = [
+  asyncRoutes['About'],
+  asyncRoutes['Detail'],
+   // es6-promise-loader returns a function
 ];
 
 
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 // App
 export * from './app.component';
 export * from './app.service';
+export * from './app.routes';
 
 import { AppState } from './app.service';
 
