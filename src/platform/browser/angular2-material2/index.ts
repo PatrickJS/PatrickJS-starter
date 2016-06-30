d7a66b5a76fcb21d4c230d0c7291adb6932063d4
dweitz43 <dweitz43@gmail.com>
2016-06-30 13:57:19 -0400

    update material to alpha.6

--- src/platform/browser/angular2-material2/index.ts
+++ src/platform/browser/angular2-material2/index.ts
@@ -7,7 +7,7 @@ import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MdProgressBar } from '@angular2-material/progress-bar';
 import { MdProgressCircle, MdSpinner } from '@angular2-material/progress-circle';
-import { MdRadioButton, MdRadioDispatcher, MdRadioGroup } from '@angular2-material/radio';
+import { MdRadioButton, MdUniqueSelectionDispatcher, MdRadioGroup } from '@angular2-material/radio';
 import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 import { MD_SLIDE_TOGGLE_DIRECTIVES } from '@angular2-material/slide-toggle';
 import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs';
@@ -44,5 +44,5 @@ export const MATERIAL_DIRECTIVES = [
 
 export const MATERIAL_PROVIDERS = [
   MdIconRegistry,
-  MdRadioDispatcher
+  MdUniqueSelectionDispatcher
 ];
