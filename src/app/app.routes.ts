8456247697879b0e81eab10c388a816da22559fb
PatrickJS <github@gdi2290.com>
2016-06-30 09:10:46 -0700

    fix: initial load of async child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,11 @@ export const routes: RouterConfig = [
   // make sure you match the component type string to the require in asyncRoutes
   { path: 'about', component: 'About' },
   // async components with children routes must use WebpackAsyncRoute
-  { path: 'detail', component: 'Detail', canActivate: [ WebpackAsyncRoute ] },
+  { path: 'detail', component: 'Detail',
+    canActivate: [ WebpackAsyncRoute ],
+    children: [
+      { path: '', component: 'Index' }  // must be included
+    ]},
   { path: '**',    component: NoContent },
 ];
 
@@ -21,6 +25,7 @@ export const asyncRoutes: AsyncRoutes = {
   // we have to use the alternative syntax for es6-promise-loader to grab the routes
   'About': require('es6-promise-loader!./about'),
   'Detail': require('es6-promise-loader!./+detail'),
+  'Index': require('es6-promise-loader!./+detail'), // must be exported with detail/index.ts
 };
 
 
