dd106204e92b9a3cdca0495eb86a70144cb4b4f0
PatrickJS <github@gdi2290.com>
2016-07-30 14:29:46 -0700

    feat: use @angularclass/conventions-loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -140,7 +140,7 @@ module.exports = {
        */
       {
         test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
+        loaders: ['awesome-typescript-loader', '@angularclass/conventions-loader'],
         exclude: [/\.(spec|e2e)\.ts$/]
       },
 
