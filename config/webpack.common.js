227138c2b038d1852feb0224ab27624e3bbad038
PatrickJS <github@gdi2290.com>
2016-07-30 18:44:13 -0700

    Update webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -140,7 +140,7 @@ module.exports = {
        */
       {
         test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', '@angularclass/conventions-loader'],
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
         exclude: [/\.(spec|e2e)\.ts$/]
       },
 
