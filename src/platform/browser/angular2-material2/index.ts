5006f04083468c1182747c0607316fc398c393ec
BIZID Amine <amine.bizid@gmail.com>
2016-08-11 19:05:14 +0200

    Add portal directives

--- src/platform/browser/angular2-material2/index.ts
+++ src/platform/browser/angular2-material2/index.ts
@@ -12,7 +12,7 @@ import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 import { MD_SLIDE_TOGGLE_DIRECTIVES } from '@angular2-material/slide-toggle';
 import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs';
 import { MdToolbar } from '@angular2-material/toolbar';
-import { MD_RIPPLE_DIRECTIVES } from '@angular2-material/core';
+import { MD_RIPPLE_DIRECTIVES, PORTAL_DIRECTIVES } from '@angular2-material/core';
 /*
  * we are grouping the module so we only need to manage the imports in one location
  */
@@ -41,7 +41,8 @@ export const MATERIAL_DIRECTIVES = [
   ...MD_SIDENAV_DIRECTIVES,
   ...MD_SLIDE_TOGGLE_DIRECTIVES,
   ...MD_TABS_DIRECTIVES,
-  ...MD_RIPPLE_DIRECTIVES
+  ...MD_RIPPLE_DIRECTIVES,
+  ...PORTAL_DIRECTIVES
 ];
 
 export const MATERIAL_PROVIDERS = [
