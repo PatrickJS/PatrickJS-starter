ba37a6d5ce75feca377ef94f41fbc096eb7f399b
PatrickJS <github@gdi2290.com>
2016-08-21 05:03:41 -0700

    Feature/ng2 rc5 (#875)
    * Version bump to rc.5
    
    * Migrates to RC 5: adds AppModule, re-organizes providers
    
    * Migrates to RC5: re-organize routing for async/lazy routes. still an issue though

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+import { PLATFORM_PROVIDERS } from '../platform/browser';
+import { ENV_PROVIDERS } from '../platform/environment';
+import { ROUTES, ROUTING_PROVIDERS } from './app.routes';
+
+// App is our top level component
+import { App } from './app.component';
+import { APP_RESOLVER_PROVIDERS } from './app.resolver';
+import { AppState } from './app.service';
+import { Home } from './home';
+import { NoContent } from './no-content';
+
+// Application wide providers
+const APP_PROVIDERS = [
+  ...APP_RESOLVER_PROVIDERS,
+  AppState
+];
+
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
+@NgModule({
+  bootstrap: [ App ],
+  declarations: [
+    App,
+    Home,
+    NoContent
+  ],
+  imports: [ // import Angular's modules
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
+  ],
+  providers: [ // expose our Services and Providers into Angular's dependency injection
+    PLATFORM_PROVIDERS,
+    ENV_PROVIDERS,
+    ROUTING_PROVIDERS,
+    APP_PROVIDERS
+  ]
+})
+export class AppModule {
+}
