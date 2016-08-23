c7cf01c107afdcc457d265ac5ed3dea990f01381
PatrickJS <github@gdi2290.com>
2016-08-23 00:33:36 -0700

    fix: async, hmr, remove platform

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -2,8 +2,9 @@
  * Angular bootstraping
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { decorateComponentRef } from './platform/environment';
+import { decorateModuleRef } from './app/environment';
 import { ApplicationRef } from '@angular/core';
+import { bootloader } from '@angularclass/hmr';
 /*
  * App Module
  * our top level module that holds all of our components
@@ -13,40 +14,14 @@ import { AppModule } from './app';
 /*
  * Bootstrap our Angular app with a top level NgModule
  */
-export function main(initialHmrState?: any): Promise<any> {
-
+export function main(): Promise<any> {
   return platformBrowserDynamic()
     .bootstrapModule(AppModule)
-    .then((modRef: any) => {
-      var appRef = modRef.injector.get(ApplicationRef);
-      return appRef.components[0];
-    })
-    .then(decorateComponentRef)
+    .then(decorateModuleRef)
     .catch(err => console.error(err));
 
 }
 
 
+bootloader(main);
 
-
-
-/*
- * Vendors
- * For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
- * You can also import them in vendors to ensure that they are bundled in one file
- * Also see custom-typings.d.ts as you also need to do `typings install x` where `x` is your module
- */
-
-
-/*
- * Hot Module Reload
- * experimental version by @gdi2290
- */
-if ('development' === ENV && HMR === true) {
-  // activate hot module reload
-  let ngHmr = require('angular2-hmr');
-  ngHmr.hotModuleReplacement(main, module);
-} else {
-  // bootstrap when document is ready
-  document.addEventListener('DOMContentLoaded', () => main());
-}
