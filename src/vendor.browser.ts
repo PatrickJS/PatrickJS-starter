edbf2f9f78bbeb66b68cf8d010bbf5a73a60c135
PatrickJS <github@gdi2290.com>
2016-08-25 10:37:48 -0700

    Update vendor.browser.ts

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -3,6 +3,8 @@
 // at the entry point of the async loaded file. Also see custom-typings.d.ts as you also need to
 // run `typings install x` where `x` is your module
 
+// TODO(gdi2290): switch to DLLs
+
 // Angular 2
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
