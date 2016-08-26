d59be2b28f2f95659c48f052f55c67667769363b
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-08-26 19:53:56 +0200

    fix: TestComponentBuilder  (#904)

--- src/app/about/about.spec.ts
+++ src/app/about/about.spec.ts
@@ -1,4 +1,4 @@
-import { TestComponentBuilder } from '@angular/compiler/testing';
+import { TestComponentBuilder } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {
