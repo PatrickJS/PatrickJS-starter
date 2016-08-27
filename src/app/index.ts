e54bd6ba3a46d754d193f26c47aa000d08189e2b
dweitz43 <dweitz43@gmail.com>
2016-08-28 01:18:16 +0300

    update material2 branch

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,2 @@
 // App
-export * from './app.component';
-export * from './app.service';
-export * from './app.routes';
-
-import { AppState } from './app.service';
-
-// Application wide providers
-export const APP_PROVIDERS = [
-  AppState
-];
+export * from './app.module';
