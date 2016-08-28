026abb3840cd5c8f80303566aed6e3f92ede745a
dweitz43 <dweitz43@gmail.com>
2016-08-28 11:51:11 +0300

    remove material imports from vendor bundle as they are included in main bundle

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -14,29 +14,6 @@ import '@angular/forms';
 import '@angular/http';
 import '@angular/router';
 
-// Angular2 Material
-import '@angular2-material/core';
-import '@angular2-material/button';
-import '@angular2-material/button-toggle';
-import '@angular2-material/card';
-import '@angular2-material/checkbox';
-import '@angular2-material/grid-list';
-import '@angular2-material/icon';
-import '@angular2-material/input';
-import '@angular2-material/list';
-import '@angular2-material/menu';
-import '@angular2-material/progress-bar';
-import '@angular2-material/progress-circle';
-import '@angular2-material/radio';
-import '@angular2-material/sidenav';
-import '@angular2-material/slide-toggle';
-import '@angular2-material/slider';
-import '@angular2-material/tabs';
-import '@angular2-material/toolbar';
-import '@angular2-material/tooltip';
-
-
-
 // Hammer.js
 // We need to import this library in order for Material to work
 // Material needs this for md-tooltips
