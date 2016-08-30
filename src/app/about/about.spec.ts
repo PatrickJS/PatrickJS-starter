13250b813fc2546a970a878504ca7d380a2f0329
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-08-30 19:49:10 +0200

    Replace obsolete addProviders by TestBed.configureTestingModule
    Replace obsolete addProviders by TestBed.configureTestingModule

--- src/app/about/about.spec.ts
+++ src/app/about/about.spec.ts
@@ -1,28 +1,29 @@
-import { TestComponentBuilder } from '@angular/core/testing';
+import { TestComponentBuilder, addProviders, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing/test_bed';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {
-  addProviders,
-  inject
-} from '@angular/core/testing';
 
 // Load the implementations that should be tested
 import { About } from './about.component';
 
 describe('About', () => {
   // provide our implementations or mocks to the dependency injector
-  beforeEach(() => addProviders([
-    // provide a better mock
-    {
-      provide: ActivatedRoute,
-      useValue: {
-        data: {
-          subscribe: (fn) => fn({yourData: 'yolo'})
-        }
-      }
-    },
-    About
-  ]));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+          // provide a better mock
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              data: {
+                subscribe: (fn) => fn({yourData: 'yolo'})
+              }
+            }
+          },
+          About
+      ]
+    });
+  });
 
   it('should log ngOnInit', inject([ About ], (about) => {
     spyOn(console, 'log');
--- src/app/home/title/title.spec.ts
+++ src/app/home/title/title.spec.ts
@@ -1,29 +1,28 @@
-import {
-  addProviders,
-  inject
-} from '@angular/core/testing';
-import { TestComponentBuilder } from '@angular/core/testing';
+import { TestComponentBuilder, addProviders, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing/test_bed';
 import { Component } from '@angular/core';
-import { BaseRequestOptions, Http } from '@angular/http';
+import { BaseRequestOptions, Http, ConnectionBackend } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 
 import { Title } from './title.service';
 
 describe('Title', () => {
-  beforeEach(() => addProviders([
-    BaseRequestOptions,
-    MockBackend,
-    {
-      provide: Http,
-      useFactory: function(backend, defaultOptions) {
-        return new Http(backend, defaultOptions);
-      },
-      deps: [MockBackend, BaseRequestOptions]
-    },
-
-    Title
-  ]));
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        Title
+      ]
+    });
+  });
 
   it('should have http', inject([ Title ], (title) => {
     expect(!!title.http).toEqual(true);
