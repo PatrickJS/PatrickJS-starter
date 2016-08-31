e1ac7b905f6503364ff1c8012cc9a1c1a44a313f
Sebastian Döll <sebastian@pixelmilk.com>
2016-09-01 00:32:50 +0200

    ~ doing some linting in specs

--- src/app/about/about.spec.ts
+++ src/app/about/about.spec.ts
@@ -1,31 +1,35 @@
-import { TestComponentBuilder, addProviders, inject } from '@angular/core/testing';
-import { TestBed } from '@angular/core/testing/test_bed';
-import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import {
+  addProviders,
+  inject,
+  TestComponentBuilder
+} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing/test_bed';
 
 // Load the implementations that should be tested
 import { About } from './about.component';
 
 describe('About', () => {
   // provide our implementations or mocks to the dependency injector
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-          // provide a better mock
-          {
-            provide: ActivatedRoute,
-            useValue: {
-              data: {
-                subscribe: (fn) => fn({yourData: 'yolo'})
-              }
-            }
-          },
-          About
-      ]
-    });
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      // provide a better mock
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          data: {
+            subscribe: (fn) => fn({
+              yourData: 'yolo'
+            })
+          }
+        }
+      },
+      About
+    ]
+  }));
 
-  it('should log ngOnInit', inject([ About ], (about) => {
+  it('should log ngOnInit', inject([About], (about) => {
     spyOn(console, 'log');
     expect(console.log).not.toHaveBeenCalled();
 
--- src/app/home/title/title.spec.ts
+++ src/app/home/title/title.spec.ts
@@ -1,28 +1,33 @@
-import { TestComponentBuilder, addProviders, inject } from '@angular/core/testing';
+import {
+  addProviders,
+  inject,
+  TestComponentBuilder
+} from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing/test_bed';
 import { Component } from '@angular/core';
-import { BaseRequestOptions, Http, ConnectionBackend } from '@angular/http';
+import {
+  BaseRequestOptions,
+  ConnectionBackend,
+  Http
+} from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 
 import { Title } from './title.service';
 
 describe('Title', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        BaseRequestOptions,
-        MockBackend,
-        {
-          provide: Http,
-          useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
-            return new Http(backend, defaultOptions);
-          },
-          deps: [MockBackend, BaseRequestOptions]
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      BaseRequestOptions,
+      MockBackend,
+      {
+        provide: Http,
+        useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
+          return new Http(backend, defaultOptions);
         },
-        Title
-      ]
-    });
-  });
+        deps: [MockBackend, BaseRequestOptions]
+      },
+      Title
+    ]}));
 
   it('should have http', inject([ Title ], (title) => {
     expect(!!title.http).toEqual(true);
