1750bb40f452aabba198b48d3944f938cbb61e8e
dweitz43 <dweitz43@gmail.com>
2016-09-01 09:42:24 +0300

    Merge branch 'master' of https://github.com/dweitz43/angular2-webpack-starter into material2

--- README.md
+++ README.md
@@ -133,6 +133,8 @@ What you need to run this app:
 * `node` and `npm` (`brew install node`)
 * Ensure you're running the latest versions Node `v4.x.x`+ (or `v5.x.x`) and NPM `3.x.x`+
 
+> If you have `nvm` installed, which is highly recommended (`brew install nvm`) you can do a `nvm install --lts && nvm use` in `$` to run with the latest Node LTS. You can also have this `zsh` done for you [automatically](https://github.com/creationix/nvm#calling-nvm-use-automatically-in-a-directory-with-a-nvmrc-file) 
+
 Once you have those, you should install these globals with `npm install --global`:
 * `webpack` (`npm install --global webpack`)
 * `webpack-dev-server` (`npm install --global webpack-dev-server`)
--- src/app/about/about.spec.ts
+++ src/app/about/about.spec.ts
@@ -1,30 +1,35 @@
-import { TestComponentBuilder } from '@angular/core/testing';
-import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 import {
   addProviders,
-  inject
+  inject,
+  TestComponentBuilder
 } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing/test_bed';
 
 // Load the implementations that should be tested
 import { About } from './about.component';
 
 describe('About', () => {
   // provide our implementations or mocks to the dependency injector
-  beforeEach(() => addProviders([
-    // provide a better mock
-    {
-      provide: ActivatedRoute,
-      useValue: {
-        data: {
-          subscribe: (fn) => fn({yourData: 'yolo'})
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      // provide a better mock
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          data: {
+            subscribe: (fn) => fn({
+              yourData: 'yolo'
+            })
+          }
         }
-      }
-    },
-    About
-  ]));
+      },
+      About
+    ]
+  }));
 
-  it('should log ngOnInit', inject([ About ], (about) => {
+  it('should log ngOnInit', inject([About], (about) => {
     spyOn(console, 'log');
     expect(console.log).not.toHaveBeenCalled();
 
