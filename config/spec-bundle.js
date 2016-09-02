fad28acfd89b63ea1b4e7534289ded99e3432448
Sebastian Döll <sebastian@pixelmilk.com>
2016-09-02 08:43:11 +0200

    Updating to angular2-*@2.0.0-rc6 (deps, specs ...) (#944)
    * ~ adding angular-*@2.0.0-rc6
    * ~ fixing .specs for angular-*@2.0.0-rc6
    * ~ removing comment from x-large spec
    * ~ remove old module from +detail and adding again CommonModule
    * ~ disabling DedupePlugin (https://github.com/angular/angular-cli/issues/1587)

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -21,11 +21,11 @@ require('ts-helpers');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy'); // since zone.js 0.6.15
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy'); // since zone.js 0.6.15
-require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 
 // RxJS
 require('rxjs/Rx');
