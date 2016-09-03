648e1b7f037f76412860761eb8a8f6f44a5e28a9
zigzag95 <amine.bizid@gmail.com>
2016-09-03 12:00:36 +0200

    build dll

--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -121,3 +121,51 @@ interface ErrorConstructor extends ErrorStackTraceLimit {}
 interface NodeRequireFunction extends Es6PromiseLoader  {}
 interface NodeModule extends WebpackModule {}
 interface Global extends GlobalEnvironment  {}
+
+
+type EnvOptions = any;
+
+
+interface WebpackConfig {
+  cache?: boolean;
+  target?: string;
+  devtool?: string;
+  entry: Entry;
+  output: any;
+  module?: any;
+  // module?: {
+  //   preLoaders?: Array<any>;
+  //   loaders?: Array<any>;
+  //   postLoaders?: Array<any>
+  // };
+  plugins?: Array<any>;
+  resolve?: {
+    unsafeCache?: boolean | Array<string>;
+    root?: string;
+    extensions?: Array<string>;
+  };
+  devServer?: {
+    staticOptions?: any;
+    setup?: (app: any, fs?: any) => any;
+    compress?: boolean;
+    quiet?: boolean,
+    contentBase?: string;
+    port?: number;
+    historyApiFallback?: boolean;
+    hot?: boolean;
+    inline?: boolean;
+    host?: string;
+    https?: boolean;
+  };
+  node?: {
+    process?: boolean;
+    global?: boolean | string;
+    Buffer?: boolean;
+    crypto?: string | boolean;
+    module?: boolean;
+    clearImmediate?: boolean;
+    setImmediate?: boolean
+    clearTimeout?: boolean;
+    setTimeout?: boolean
+  };
+}
---
+++ src/dll.ts
@@ -0,0 +1,72 @@
+/*
+ * README
+ * any changes to this file and you have to run `npm run dll` to generate the bundle
+ *
+ * Polyfills
+ * Vendors
+ * RxJS
+ */
+
+// Polyfills
+export function polyfills(env?: any) {
+  return [
+    // 'ie-shim',
+
+    'core-js/es6/symbol',
+    'core-js/es6/object',
+    'core-js/es6/function',
+    'core-js/es6/parse-int',
+    'core-js/es6/parse-float',
+    'core-js/es6/number',
+    'core-js/es6/math',
+    'core-js/es6/string',
+    'core-js/es6/date',
+    'core-js/es6/array',
+    'core-js/es6/regexp',
+    'core-js/es6/map',
+    'core-js/es6/set',
+    'core-js/es6/weak-map',
+    'core-js/es6/weak-set',
+    'core-js/es6/typed',
+    'core-js/es6/reflect',
+    // 'core-js/es6/promise', // problem with firefox
+    'core-js/es7/reflect',
+
+    // zone.js
+    'zone.js/dist/zone',
+    'zone.js/dist/long-stack-trace-zone',
+
+    // typescript helpers
+    'ts-helpers',
+  ];
+}
+
+// Angular 2 and other Vendor imports
+export function vendors(env?: any) {
+  return [
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/compiler',
+    '@angular/router',
+    '@angular/forms',
+    '@angular/common',
+    '@angular/core',
+    '@angular/http',
+
+    '@angularclass/form-validators',
+    '@angularclass/hmr',
+  ];
+}
+
+// RxJS
+export function rxjs(env?: any) {
+  return [
+    'rxjs/Observable',
+    'rxjs/Subscription',
+    'rxjs/Subject',
+    'rxjs/BehaviorSubject',
+    'rxjs/add/operator/map',
+    'rxjs/add/operator/mergeMap',
+    'rxjs/add/operator/distinctUntilChanged',
+  ];
+}
