cb4eec324ade554d3e1085ef4e6c026f07935886
PatrickJS <github@gdi2290.com>
2016-09-08 20:13:01 -0700

    fix(webpack): check if default for es5 module

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -100,7 +100,7 @@ module.exports = {
         loader: 'string-replace-loader',
         query: {
           search: '(System|SystemJS)(.*[\\n\\r]\\s*\\.|\\.)import\\((.+)\\)',
-          replace: '$1.import($3).then(mod => mod.__esModule ? mod.default : mod)',
+          replace: '$1.import($3).then(mod => mod.__esModule ? (mod.default || mod) : mod)',
           flags: 'g'
         },
         include: [helpers.root('src')]
