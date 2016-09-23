57b6c0c941c33a272aad1cb5ceeaa1c586263256
Ashley L Hill <ashleahhill@gmail.com>
2016-09-23 04:52:37 -0400

    Set karma-webpack to only log errors (#1005)
    Set wepack to only output errors during testing

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     },
 
     // Webpack please don't spam the console when running in karma!
-    webpackServer: { noInfo: true },
+    webpackMiddleware: { stats: 'errors-only'},
 
     /*
      * test results reporter to use
