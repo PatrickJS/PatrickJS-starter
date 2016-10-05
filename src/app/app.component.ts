feaff5522e913fd86d65007884640b845ba24627
David Herges <david@spektrakel.de>
2016-10-05 09:31:02 +0200

    Embraces ng2 styleguide *.component.{css|html|ts} (#1084)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { AppState } from './app.service';
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
-    './app.style.css'
+    './app.component.css'
   ],
   template: `
     <nav>
