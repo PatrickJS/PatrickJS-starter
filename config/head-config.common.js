beb83e5dc314fe462a927eaec068c5f4748a7a86
Elle Beal <ebeal@osisoft.com>
2016-11-11 13:25:28 -0800

    update comments

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -25,6 +25,8 @@ module.exports = {
     { rel: 'apple-touch-icon', sizes: '144x144', href: '/assets/icon/apple-icon-144x144.png' },
     { rel: 'apple-touch-icon', sizes: '152x152', href: '/assets/icon/apple-icon-152x152.png' },
     { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/icon/apple-icon-180x180.png' },
+
+    /** <link> tags to the material design assets */
     { rel: 'stylesheet', href : 'https://unpkg.com/@angular/material/core/theming/prebuilt/indigo-pink.css' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
 
