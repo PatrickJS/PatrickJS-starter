1419820c32b30c74144d7087b3e3bc09ef49a3fe
Shlomi Assaf <shlomiassaf@users.noreply.github.com>
2016-11-22 16:25:27 +0200

    fix: github-deploy fix using webpack 2 (#1089)
    Webpack 2 does not accept arbitrary cli arguments, this is why github script fails.
    
    This PR moves to a file based env selection.
    
    closes #1078

--- config/webpack.github-deploy.js
+++ config/webpack.github-deploy.js
@@ -1,11 +1,12 @@
 /**
  * @author: @AngularClass
  */
+const fs = require('fs');
+const path = require('path');
 const helpers = require('./helpers');
 const ghDeploy = require('./github-deploy');
 const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 const ghpages = require('gh-pages');
-const webpackConfig = ghDeploy.getWebpackConfigModule(); // the settings that are common to prod and dev
 
 
 /**
@@ -15,65 +16,63 @@ const GIT_REMOTE_NAME = 'origin';
 const COMMIT_MESSAGE = 'Updates';
 const GH_REPO_NAME = ghDeploy.getRepoName(GIT_REMOTE_NAME);
 
-const METADATA = webpackMerge(webpackConfig.metadata, {
-  /**
-   * Prefixing the REPO name to the baseUrl for router support.
-   * This also means all resource URIs (CSS/Images/JS) will have this prefix added by the browser
-   * unless they are absolute (start with '/'). We will handle it via `output.publicPath`
-   */
-  baseUrl: '/' + GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.metadata.baseUrl)
-});
+module.exports = function (options) {
+  const webpackConfigFactory = ghDeploy.getWebpackConfigModule(options); // the settings that are common to prod and dev
+  const webpackConfig = webpackConfigFactory(options);
 
-module.exports = webpackMerge(webpackConfig, {
-  /**
-   * Merged metadata from webpack.common.js for index.html
-   *
-   * See: (custom attribute)
-   */
-  metadata: METADATA,
+  // replace the instance of HtmlWebpackPlugin with an updated one.
+  ghDeploy.replaceHtmlWebpackPlugin(webpackConfig.plugins, GH_REPO_NAME);
 
+  return webpackMerge(webpackConfig, {
+   output: {
+     /**
+      * The public path is set to the REPO name.
+      *
+      * `HtmlElementsPlugin` will add it to all resources url's created by it.
+      * `HtmlWebpackPlugin` will add it to all webpack bundels/chunks.
+      *
+      * In theory publicPath shouldn't be used since the browser should automatically prefix the
+      * `baseUrl` into all URLs, however this is not the case when the URL is absolute (start with /)
+      *
+      * It's important to prefix & suffix the repo name with a slash (/).
+      * Prefixing so every resource will be absolute (otherwise it will be url.com/repoName/repoName...
+      * Suffixing since chunks will not do it automatically (testes against about page)
+      */
+     publicPath: '/' + GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.output.publicPath)
+   },
 
-  output: {
-    /**
-     * The public path is set to the REPO name.
-     *
-     * `HtmlElementsPlugin` will add it to all resources url's created by it.
-     * `HtmlWebpackPlugin` will add it to all webpack bundels/chunks.
-     *
-     * In theory publicPath shouldn't be used since the browser should automatically prefix the
-     * `baseUrl` into all URLs, however this is not the case when the URL is absolute (start with /)
-     *
-     * It's important to prefix & suffix the repo name with a slash (/).
-     * Prefixing so every resource will be absolute (otherwise it will be url.com/repoName/repoName...
-     * Suffixing since chunks will not do it automatically (testes against about page)
-     */
-    publicPath: '/' + GH_REPO_NAME + '/' + ghDeploy.safeUrl(webpackConfig.output.publicPath)
-  },
+   plugins: [
+     function() {
+       this.plugin('done', function(stats) {
+         console.log('Starting deployment to GitHub.');
 
-  plugins: [
-    function() {
-      this.plugin('done', function(stats) {
-        console.log('Starting deployment to GitHub.');
+         const logger = function (msg) {
+           console.log(msg);
+         };
 
-        const logger = function (msg) {
-          console.log(msg);
-        };
+         const options = {
+           logger: logger,
+           remote: GIT_REMOTE_NAME,
+           message: COMMIT_MESSAGE,
+           dotfiles: true // for .nojekyll
+         };
 
-        const options = {
-          logger: logger,
-          remote: GIT_REMOTE_NAME,
-          message: COMMIT_MESSAGE
-        };
+         // Since GitHub moved to Jekyll 3.3, their server ignores the "node_modules" and "vendors" folder by default.
+         // but, as of now, it also ignores "vendors*" files.
+         // This means vendor.bundle.js or vendor.[chunk].bundle.js will return 404.
+         // this is the fix for now.
+         fs.writeFileSync(path.join(webpackConfig.output.path, '.nojekyll'), '');
 
-        ghpages.publish(webpackConfig.output.path, options, function(err) {
-          if (err) {
-            console.log('GitHub deployment done. STATUS: ERROR.');
-            throw err;
-          } else {
-            console.log('GitHub deployment done. STATUS: SUCCESS.');
-          }
-        });
-      });
-    }
-  ]
-});
+         ghpages.publish(webpackConfig.output.path, options, function(err) {
+           if (err) {
+             console.log('GitHub deployment done. STATUS: ERROR.');
+             throw err;
+           } else {
+             console.log('GitHub deployment done. STATUS: SUCCESS.');
+           }
+         });
+       });
+     }
+   ]
+ });
+};
