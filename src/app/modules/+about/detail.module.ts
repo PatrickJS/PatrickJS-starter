347d5b4397903d7caa74bdd669f2a5c0f333f49e
Arturo Zarzalejo <Arturoazh@gmail.com>
2016-11-24 17:25:39 +0100

    chore()
    - Add youtube API

--- src/app/modules/+about/detail.module.ts
+++ src/app/modules/+about/detail.module.ts
@@ -1,19 +1,26 @@
+import { HttpModule, JsonpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MaterialModule } from '@angular/material';
 import { DetailComponent } from './detail.component';
 import { routes } from './detail.routes';
 import { FsDialogComponent } from '../../shared/components/fs-dialog';
+import { FsYoutubeAPIComponent } from '../../shared/components/fs-youtube-api';
 
 @NgModule({
   declarations: [
     DetailComponent,
-    FsDialogComponent
+    FsDialogComponent,
+    FsYoutubeAPIComponent
   ],
   imports: [
     CommonModule,
+    HttpModule,
+    JsonpModule,
     RouterModule.forChild(routes),
+    MaterialModule.forRoot()
   ]
 })
 
