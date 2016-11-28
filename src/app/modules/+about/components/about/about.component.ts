db6ddbd460816a5932e8b25e34c2f2576fe7fe8b
Arturo Zarzalejo <Arturoazh@gmail.com>
2016-11-28 17:12:18 +0100

    chore()

--- src/app/modules/+about/components/about/about.component.ts
+++ src/app/modules/+about/components/about/about.component.ts
@@ -1,14 +1,28 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { FsUsersService } from '../fs-users/fs-users.service.ts';
 
 @Component({
   selector: 'about',
+  providers: [FsUsersService],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
 
 export class AboutComponent {
-  constructor() {}
+  dataUsers
 
-  ngOnInit() {}
+  constructor(private usersService: FsUsersService) {
+    // console.log(usersService);
+  }
+
+  ngOnInit() {
+    this.usersService
+    .search()
+    .subscribe(
+      data => {
+        console.log('datos usuarios', data);
+        this.dataUsers = data;
+    });
+  }
 
 }
