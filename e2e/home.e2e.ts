42f04d1dab740f19e407b4bf91395a86ea193e17
dvdchavarri <dvdchavarri@gmail.com>
2016-11-30 16:51:12 +0100

    fix() e2e test

--- e2e/home.e2e.ts
+++ e2e/home.e2e.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
 
-describe('App', () => {
+describe('Home', () => {
 
   beforeEach(() => {
     // change hash depending on router LocationStrategy
