d5be8194d3a927627e856129013568516a58395c
Jónatan Núñez <jnunez@bebee.com>
2017-01-31 10:21:31 +0100

    Use tslib instead of ts-helpers

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -16,9 +16,6 @@ Error.stackTraceLimit = Infinity;
 require('core-js/es6');
 require('core-js/es7/reflect');
 
-// Typescript emit helpers polyfill
-require('ts-helpers');
-
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/proxy'); // since zone.js 0.6.15
