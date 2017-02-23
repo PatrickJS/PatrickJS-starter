7f044a1ed62948f5da79f5d1356cfb22090ac196
vjcspy <mr.vjcspy@gmail.com>
2017-02-23 16:37:05 +0700

    - create superadmin account when start app - define way to map data to Class

---
+++ cloud/server/core/DataObject.ts
@@ -0,0 +1,49 @@
+import * as _ from "lodash";
+export class DataObject extends Object {
+  // abstract identify: string;
+  
+  static create<T>(data: Object): T {
+    return Object.create(this.prototype).addData(data);
+  }
+  
+  getFirst(): any {
+    return this[0];
+  }
+  
+  getData(key?: string): any {
+    if (typeof key == "undefined")
+      return this;
+    
+    let strKey = key + '';
+    if (this.hasOwnProperty(strKey))
+      return this[strKey];
+    else
+      return null;
+  }
+  
+  setData(key, value): any {
+    let strKey   = key + '';
+    this[strKey] = value;
+    return this;
+  }
+  
+  hasData(key): boolean {
+    let strKey = key + '';
+    return this.hasOwnProperty(strKey) && this[strKey] != null;
+  }
+  
+  unsetData(key): any {
+    if (this.hasData(key))
+      delete this[key];
+    return this;
+  }
+  
+  addData(data: Object): any {
+    _.forEach(
+      data, (v, k) => {
+        let strKey   = k + '';
+        this[strKey] = v;
+      });
+    return this;
+  }
+}
--- cloud/server/main.ts
+++ cloud/server/main.ts
@@ -1,4 +1,27 @@
 import {Meteor} from 'meteor/meteor';
+import {Users} from "./collections/users";
+import {User} from "./models/user";
+import {Role} from "./models/role";
+
 Meteor.startup(() => {
-    // code to run on server at startup
+  initSupperAdminAccount();
+  createIndexCollection();
 });
+
+let initSupperAdminAccount = () => {
+  let _superUser = () => {
+    return Users.findOne({"username": "superadmin"})
+  };
+  if (!_superUser()) {
+    Accounts.createUser(
+      {
+        username: "superadmin",
+        email: "khoild@smartosc.com",
+        password: "admin123"
+      });
+  }
+  User.create<User>(_superUser()).addToRoles(Role.SUPERADMIN, Role.GROUP_CLOUD);
+};
+
+let createIndexCollection = () => {
+};
