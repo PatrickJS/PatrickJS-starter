ccde769e587218a5760389ef6559b682e8dd5bcf
vjcspy <mr.vjcspy@gmail.com>
2017-02-25 20:07:22 +0700

    - users collection not integrate with schema

--- cloud/server/collections/Users.ts
+++ cloud/server/collections/Users.ts
@@ -3,47 +3,4 @@ import SimpleSchema from 'simpl-schema';
 import {CollectionMaker} from "./Contract/CollectionMaker";
 import {DateTimeHelper} from "../code/DateTimeHelper";
 
-export const Users = CollectionMaker.makeFromExisting<UserInterface>(Meteor.users,
-                                                                     new SimpleSchema(
-                                                                       {
-                                                                         username: {
-                                                                           type: String,
-                                                                           min: 6,
-                                                                           max: 40
-                                                                         },
-                                                                         email: {
-                                                                           type: String,
-                                                                           regEx: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                                                                         },
-                                                                         password: {
-                                                                           type: String,
-                                                                           min: 6,
-                                                                           max: 50
-                                                                         },
-                                                                         license: {
-                                                                           type: String
-                                                                         },
-                                                                         profile: {
-                                                                           type: Object,
-                                                                           optional: true
-                                                                         },
-                                                                         'profile.first_name': {
-                                                                           type: String,
-                                                                           optional: true
-                                                                         },
-                                                                         'profile.last_name': {
-                                                                           type: String,
-                                                                           optional: true
-                                                                         },
-                                                                       },
-                                                                       {
-                                                                         clean: {
-                                                                           filter: true,
-                                                                           autoConvert: true,
-                                                                           removeEmptyStrings: true,
-                                                                           trimStrings: true,
-                                                                           getAutoValues: true,
-                                                                           removeNullsFromArrays: true,
-                                                                         }
-                                                                       }
-                                                                     ));
+export const Users = CollectionMaker.makeFromExisting<UserInterface>(Meteor.users);
