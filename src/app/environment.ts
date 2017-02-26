9ba5bb80e9e3b58d6394b195910a0d3fc9351890
SquadWuschel <Joecorporation@gmx.de>
2017-02-26 22:12:14 +0100

    added complete "path" for ENV Mode
    when I tried to use this example to implement this into a Visual Studio then only "ENV" was not found and I can't compile the project.

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -16,7 +16,7 @@ let PROVIDERS: any[] = [
 // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
 let _decorateModuleRef = <T>(value: T): T => { return value; };
 
-if ('production' === ENV) {
+if ('production' === process.env.ENV) {
   enableProdMode();
 
   // Production
