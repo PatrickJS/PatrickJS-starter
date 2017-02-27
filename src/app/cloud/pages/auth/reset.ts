1055816bc8d4d989a55ea5866b82f0da6b6c43df
vjcspy <mr.vjcspy@gmail.com>
2017-02-27 22:53:53 +0700

    - refactor

--- src/app/cloud/pages/auth/reset.ts
+++ src/app/cloud/pages/auth/reset.ts
@@ -51,6 +51,7 @@ export class ResetPasswordComponent implements OnInit {
                                                    }
                                                  });
     };
+    initValidationReminder();
   }
   
 }
