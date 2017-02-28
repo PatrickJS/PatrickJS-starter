5c0dff9b86a8a0bd6efa49619e4a94fbd2445e27
vietlq57 <luongviet82@gmail.com>
2017-02-28 15:01:23 +0700

    dummy licenses

--- cloud/server/test/Seeder.ts
+++ cloud/server/test/Seeder.ts
@@ -1,13 +1,18 @@
 import {Products} from "../collections/Products";
 import {Product} from "../models/Product";
 import {Prices} from "../collections/Prices";
+import { Users } from '../collections/Users';
 import {OM} from "../code/General/ObjectManager";
 import {Price} from "../models/Price";
+import { License } from '../models/License';
+import { Licenses } from '../collections/Licenses';
+import {DateTimeHelper} from "../code/DateTimeHelper";
 
 export class Seeder {
   run() {
     this.dummyProduct();
     this.dummyPrices();
+    this.dummyLicenses();
   }
   
   private dummyProduct(): void {
@@ -42,5 +47,66 @@ export class Seeder {
       let _price = OM.create<Price>(Price, false, _p());
       _price.save();
     }
+  }
+
+  private dummyLicenses(){
+    if(Licenses.collection.find().count() > 0)
+      return;
+    let products = Products.find().fetch();
+    let prices = Prices.find().fetch();
+    let users = Users.find().fetch();
+    
+    let _license = () => {
+      let user_id = this.randomCheckIdObject(users);
+      let licensehasproduct = [];
+      let product_ids = [];
+      let user_ids = [];
+      for(let i=0; i < Math.round(Math.random() * 10); ++i){
+        let product_id = this.randomCheckIdObject(products);
+        let price_id = this.randomCheckIdObject(prices);
+        let base_url = [];
+        for(let j=0; j < Math.round(Math.random() * 5); ++j){
+          base_url.push(Math.random().toString(36).substring(7));
+        }
+        let start_version = "0.0." + Math.round(Math.random() * 10);
+        let purchase_date = DateTimeHelper.getCurrentDate();
+        let expired_date = DateTimeHelper.getCurrentDate();
+        
+        licensehasproduct.push({
+          product_id: product_id,
+          base_url: base_url,
+          pricing_id: price_id,
+          start_version: start_version,
+          purchase_date: purchase_date,
+          expired_date: expired_date
+        });
+      }
+
+      return {
+        key: Math.random().toString(36).substring(7),
+        status: Math.floor(Math.random() * 9) % 2,
+        shop_owner_id: user_id,
+        has_product: licensehasproduct,
+        created_by: Users.collection.findOne({"username": "superadmin"})._id
+      };
+    };
+    
+    for (let i = 0; i < 10; i++) {
+      let _l = OM.create<License>(License, false, _license());
+      _l.save();
+    }
+  }
+
+  randomCheckIdObject(array: any[]){
+    let id = this.randomIdObject(array);
+    while(array.indexOf(id)!=-1){
+      id = this.randomIdObject(array);
+    }
+    return id;
+  }
+
+  randomIdObject(arr: any[]) {
+    let item = arr[Math.floor(Math.random() * arr.length)];
+    return item._id;
   }
 }
