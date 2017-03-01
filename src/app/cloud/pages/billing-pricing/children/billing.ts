51782a0f142ec53c21822883ccb74c79aedf3b64
vietlq57 <luongviet82@gmail.com>
2017-03-01 10:56:19 +0700

    manage users

--- src/app/cloud/pages/billing-pricing/children/billing.ts
+++ src/app/cloud/pages/billing-pricing/children/billing.ts
@@ -2,17 +2,6 @@ import {
   Component,
   OnInit
 } from '@angular/core';
-<<<<<<< HEAD
-
-@Component({
-             selector: 'cloud-billing',
-             templateUrl: 'billing.html'
-           })
-export class CloudBillingComponent implements OnInit {
-  constructor() { }
-  
-  ngOnInit() { }
-=======
 import {BillingPricingService} from "../billing-pricing.service";
 
 @Component({
@@ -21,10 +10,9 @@ import {BillingPricingService} from "../billing-pricing.service";
            })
 export class CloudBillingComponent implements OnInit {
   constructor(protected billingPricingService: BillingPricingService) { }
-  
+
   ngOnInit() {
     this.billingPricingService.viewState.headerText = "Billing";
   }
->>>>>>> 36bae05e9706c2b4ecaa338119d4adb51e5d7218
-  
+
 }
--- src/app/cloud/pages/billing-pricing/children/pricing.ts
+++ src/app/cloud/pages/billing-pricing/children/pricing.ts
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-    selector: 'cloud-pricing',
-    templateUrl: 'pricing.html'
-})
-export class CloudPricingComponent implements OnInit {
-    constructor() { }
-
-    ngOnInit() { }
-    
-=======
 import {
   Component,
   OnInit
@@ -26,17 +13,16 @@ import {Observable} from "rxjs";
            })
 export class CloudPricingComponent implements OnInit {
   protected products: Observable<any>;
-  
+
   constructor(protected billingPricingService: BillingPricingService,
               protected productCollection: ProductCollection) {
   }
-  
+
   ngOnInit() {
     this.billingPricingService.viewState.headerText = "Pricing";
     this.productCollection.getCollectionObservable().subscribe((collection: MongoObservable.Collection<any>) => {
       this.products = collection.find();
     });
   }
-  
->>>>>>> 36bae05e9706c2b4ecaa338119d4adb51e5d7218
+
 }
