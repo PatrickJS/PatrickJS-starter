03486d725d43fb4deb18460f97fb53a79045e445
vjcspy <mr.vjcspy@gmail.com>
2017-03-01 16:56:21 +0700

    - done grid cashier

---
+++ src/app/cloud/pages/manage-shop/manage-shop.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ManageShopService {
+  viewState = {
+    headerText: ""
+  };
+  viewData  = {};
+  
+  constructor() { }
+  
+}
