f47eb909445a3c14f140f30e9038f4219708382f
vjcspy <mr.vjcspy@gmail.com>
2017-03-20 17:41:38 +0700

    - refactor code

--- cloud/server/methods/user/create_user.ts
+++ cloud/server/methods/user/create_user.ts
@@ -1,10 +1,6 @@
 import {User} from "../../models/User";
 import {OM} from "../../code/General/ObjectManager";
 import {Role} from "../../models/Role";
-import {UserLicense} from "../../models/ManyToMany/UserLicense";
-import {License} from "../../models/License";
-import * as _ from "lodash";
-import * as $q from "q";
 
 new ValidatedMethod({
   name: 'user.create_user',
@@ -18,11 +14,9 @@ new ValidatedMethod({
     }
   },
   run: function (data) {
-    let defer = $q.defer();
     let user = OM.create<User>(User).load(data['username'], "username");
     if (!user)
       Accounts.createUser(data);
-    return defer.promise;
   }
 });
 
