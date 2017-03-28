1ed14c63b8fa8ba635fd4e0568877333a4ff9f31
vjcspy <mr.vjcspy@gmail.com>
2017-03-28 14:17:18 +0700

    - nothing

--- up.sh
+++ up.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /home/vjcspy/sites/retail-cloud/cloud/dist/bundle
+cd /home/congnv/sites/cloud
 
-export ROOT_URL='http://xds.smartosc.com' export MAIL_URL=smtp://bot@smartosc.com:thestar0@smtp.gmail.com:465 && export MONGO_URL=mongodb://localhost:27017/cloud&& export PORT=2005 &&  node main.js
+export MAIL_URL=smtp://bot@smartosc.com:thestar0@smtp.gmail.com:465
+export MONGO_URL=mongodb://localhost:27017/cloud
+export ROOT_URL='http://xcloud.smartosc.com'
+export PORT=2005
+node main.js --port 2005
