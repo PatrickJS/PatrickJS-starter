83bc0afe189c7d087211245877d99b22648f4ad3
Amit <asuman@loggly.com>
2017-04-08 23:28:15 +0530

    refactor convert html content to pug
    -add reference of pug files

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,19 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   selector: 'about',
   styles: [`
   `],
-  template: `
-    <h1>About</h1>
-    <div>
-      For hot module reloading run
-      <pre>npm run start:hmr</pre>
-    </div>
-    <div>
-      <h3>
-        patrick@AngularClass.com
-      </h3>
-    </div>
-    <pre>this.localState = {{ localState | json }}</pre>
-  `
+  templateUrl: './about.pug'
 })
 export class AboutComponent implements OnInit {
 
--- src/app/no-content/no-content.component.ts
+++ src/app/no-content/no-content.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'no-content',
-  template: `
-    <div>
-      <h1>404: page missing</h1>
-    </div>
-  `
+ templateUrl: './no-content.pug'
 })
 export class NoContentComponent {
 
