465114701ebf275702efdb6d122efa2f607e5317
vietlq57 <luongviet82@gmail.com>
2017-04-17 10:59:10 +0700

    nothing

--- up.sh
+++ up.sh
@@ -4,6 +4,6 @@ cd /home/congnv/sites/cloud
 
 export MAIL_URL=smtp://bot@smartosc.com:thestar0@smtp.gmail.com:465
 export MONGO_URL=mongodb://localhost:27017/cloud
-export ROOT_URL='http://xcloud.smartosc.com'
+export ROOT_URL='http://accounts.connectpos.com'
 export PORT=2005
 node main.js --port 2005
