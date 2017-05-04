0e03fb94376c8cb3124ed32d9d0b25a3398e638f
Adam Lee <duo1123@gmail.com>
2017-05-04 09:43:06 -0400

    fix(hmr): fix missing flag to disable webpack-dev-server.js script

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -29,7 +29,8 @@ const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
   title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
   baseUrl: '/',
-  isDevServer: helpers.isWebpackDevServer()
+  isDevServer: helpers.isWebpackDevServer(),
+  HMR
 };
 
 /*
@@ -179,7 +180,7 @@ module.exports = function (options) {
           exclude: [helpers.root('src/index.html')]
         },
 
-        /* 
+        /*
          * File loader for supporting images, for example, in CSS files.
          */
         {
@@ -189,7 +190,7 @@ module.exports = function (options) {
 
         /* File loader for supporting fonts, for example, in CSS files.
         */
-        { 
+        {
           test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
           use: 'file-loader'
         }
