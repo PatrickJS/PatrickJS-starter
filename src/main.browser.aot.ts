5ae7959f907f4d75374e38de096918dcfdec2bb9
Scott Moss <willscottmoss@gmail.com>
2017-05-18 14:29:05 -0400

    Update main.browser.aot.ts

--- src/main.browser.aot.ts
+++ src/main.browser.aot.ts
@@ -8,7 +8,6 @@ import { decorateModuleRef } from './app/environment';
  * our top level module that holds all of our components.
  */
 import { AppModuleNgFactory } from '../compiled/src/app/app.module.ngfactory';
-
 /**
  * Bootstrap our Angular app with a top level NgModule.
  */
