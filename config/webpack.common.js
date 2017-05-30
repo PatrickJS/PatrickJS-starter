cd27c8d594067b2d9f66524e905920c86bf1e9ea
PatrickJS <github@gdi2290.com>
2017-05-30 14:20:42 -0400

    feat(webpack): include PreloadWebpackPlugin

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -22,6 +22,7 @@ const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const ngcWebpack = require('ngc-webpack');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
 
 /**
  * Webpack Constants
@@ -293,21 +294,23 @@ module.exports = function (options) {
         isProd ? { ignore: [ 'mock-data/**/*' ] } : undefined
       ),
 
-
-      /**
-       * Plugin: HtmlWebpackPlugin
-       * Description: Simplifies creation of HTML files to serve your webpack bundles.
-       * This is especially useful for webpack bundles that include a hash in the filename
-       * which changes every compilation.
+      /*
+       * Plugin: PreloadWebpackPlugin
+       * Description: Preload is a web standard aimed at improving
+       * performance and granular loading of resources.
        *
-       * See: https://github.com/ampedandwired/html-webpack-plugin
+       * See: https://github.com/GoogleChrome/preload-webpack-plugin
        */
-      new HtmlWebpackPlugin({
-        template: 'src/index.html',
-        title: METADATA.title,
-        chunksSortMode: 'dependency',
-        metadata: METADATA,
-        inject: 'head'
+      new PreloadWebpackPlugin({
+        rel: 'preload',
+        as: 'script',
+        include: ['polyfills', 'vendor', 'main'].reverse(),
+        fileBlacklist: ['.css', '.map']
+      }),
+      new PreloadWebpackPlugin({
+        rel: 'prefetch',
+        as: 'script',
+        include: 'asyncChunks'
       }),
 
       /**
@@ -318,7 +321,24 @@ module.exports = function (options) {
        * See: https://github.com/numical/script-ext-html-webpack-plugin
        */
       new ScriptExtHtmlWebpackPlugin({
-        defaultAttribute: 'defer'
+        sync: /polyfill|vendor/,
+        defaultAttribute: 'async'
+      }),
+
+      /*
+      * Plugin: HtmlWebpackPlugin
+      * Description: Simplifies creation of HTML files to serve your webpack bundles.
+      * This is especially useful for webpack bundles that include a hash in the filename
+      * which changes every compilation.
+      *
+      * See: https://github.com/ampedandwired/html-webpack-plugin
+      */
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        title: METADATA.title,
+        chunksSortMode: 'dependency',
+        metadata: METADATA,
+        inject: 'body'
       }),
 
       /**
@@ -355,8 +375,37 @@ module.exports = function (options) {
       new LoaderOptionsPlugin({}),
 
       new ngcWebpack.NgcWebpackPlugin({
+        /**
+         * If false the plugin is a ghost, it will not perform any action.
+         * This property can be used to trigger AOT on/off depending on your build target (prod, staging etc...)
+         *
+         * The state can not change after initializing the plugin.
+         * @default true
+         */
         disabled: !AOT,
         tsConfig: helpers.root('tsconfig.webpack.json'),
+        /**
+         * A path to a file (resource) that will replace all resource referenced in @Components.
+         * For each `@Component` the AOT compiler compiles it creates new representation for the templates (html, styles)
+         * of that `@Components`. It means that there is no need for the source templates, they take a lot of
+         * space and they will be replaced by the content of this resource.
+         *
+         * To leave the template as is set to a falsy value (the default).
+         *
+         * TIP: Use an empty file as an overriding resource. It is recommended to use a ".js" file which
+         * usually has small amount of loaders hence less performance impact.
+         *
+         * > This feature is doing NormalModuleReplacementPlugin for AOT compiled resources.
+         *
+         * ### resourceOverride and assets
+         * If you reference assets in your styles/html that are not inlined and you expect a loader (e.g. url-loader)
+         * to copy them, don't use the `resourceOverride` feature as it does not support this feature at the moment.
+         * With `resourceOverride` the end result is that webpack will replace the asset with an href to the public
+         * assets folder but it will not copy the files. This happens because the replacement is done in the AOT compilation
+         * phase but in the bundling it won't happen (it's being replaced with and empty file...)
+         *
+         * @default undefined
+         */
         resourceOverride: helpers.root('config/resource-override.js')
       }),
 
