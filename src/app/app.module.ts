99b1d443ed34c4870609c798a268cc80538c01fc
Atthawit Muansiri <atthawit.m@ku.th>
2017-10-13 12:33:20 +0700

    remove app service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,6 @@ import { ROUTES } from './app.routes';
 // App is our top level component
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
-import { AppState, InternalStateType } from './app.service';
 import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
@@ -43,7 +42,6 @@ import '../styles/headings.css';
 // Application wide providers
 const APP_PROVIDERS = [
   ...APP_RESOLVER_PROVIDERS,
-  AppState
 ];
 
 type StoreType = {
