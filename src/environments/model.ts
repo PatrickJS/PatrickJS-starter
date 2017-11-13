d85d07184c915568158691494b6dc047cefb5d06
Shlomi Assaf (shlassaf) <shlassaf@cisco.com>
2017-11-14 01:03:21 +0200

    make tslint happy

--- src/environments/model.ts
+++ src/environments/model.ts
@@ -4,5 +4,5 @@ export interface Environment {
   production: boolean;
   ENV_PROVIDERS: any;
   showDevModule: boolean;
-  decorateModuleRef(modRef: NgModuleRef<any>): NgModuleRef<any>
+  decorateModuleRef(modRef: NgModuleRef<any>): NgModuleRef<any>;
 }
