e0582012595ae966c572b8dc30e47d231a593b31
Maksym Lanovyy <maksym_lanovyy@epam.com>
2018-02-27 17:52:19 +0100

    Upgraded Dockerfile to implement multi-staged build. Fixed E2E tests. Fixed Travis CI build.

--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -7,7 +7,8 @@ describe('Home', () => {
     /**
      * Change hash depending on router LocationStrategy.
      */
-    await browser.get('/#/home');
+    await browser.get('/');
+    await element(by.linkText('Home')).click();
   });
 
   it('should have a title', async () => {
