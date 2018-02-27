fa8d0123637fac738324f61cfc9a54a92d7d7fde
Maksym Lanovyy <maksym_lanovyy@epam.com>
2018-02-27 15:56:54 +0100

    Added back --no-sandbox arg for Chrome to run dockerized environment of Travis CI.

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -33,7 +33,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+      args: [ "--headless", "--disable-gpu", "--window-size=800x600", "--no-sandbox" ]
     }
   },
 
