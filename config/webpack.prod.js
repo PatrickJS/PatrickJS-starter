59fad019155c0c09918cc852f7676d2ab454302a
Al-Mothafar Al-Hasan <almothafar@gmail.com>
2018-04-18 16:53:01 +0300

    Disable sourceMap by default for prod build and provide EVN option to enable it

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -49,6 +49,7 @@ function getUglifyOptions (supportES2015) {
 module.exports = function (env) {
   const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
   const supportES2015 = buildUtils.supportES2015(buildUtils.DEFAULT_METADATA.tsConfigPath);
+  const sourceMapEnabled = process.env.SOURCE_MAP === '1';
   const METADATA = Object.assign({}, buildUtils.DEFAULT_METADATA, {
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || 8080,
@@ -171,7 +172,7 @@ module.exports = function (env) {
        * NOTE: To debug prod builds uncomment //debug lines and comment //prod lines
        */
       new UglifyJsPlugin({
-        sourceMap: true,
+        sourceMap: sourceMapEnabled,
         parallel: true,
         uglifyOptions: getUglifyOptions(supportES2015)
       }),
