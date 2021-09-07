fe489ac20e155a861530a0416a996acc8740b82a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-07 21:37:56 +0800

    Delete src directory

--- src/environments/model.ts
+++
@@ -1,8 +0,0 @@
-import { NgModuleRef } from '@angular/core';
-
-export interface Environment {
-  production: boolean;
-  ENV_PROVIDERS: any;
-  showDevModule: boolean;
-  decorateModuleRef(modRef: NgModuleRef<any>): NgModuleRef<any>;
-}
